510947d40447534cabae849a87d247709a0de540
chrisAXZA <nexter1@tutanota.com>
2022-03-13 12:19:21 +0200

    Implement aside html

--- Workshop-Forum/Forum-App/src/app/core/aside/aside.component.ts
+++ Workshop-Forum/Forum-App/src/app/core/aside/aside.component.ts
@@ -1,14 +1,15 @@
 import { Component, Input } from '@angular/core';
+import { ITheme } from 'src/app/shared/interfaces';
 
 @Component({
   selector: 'app-aside',
   templateUrl: './aside.component.html',
   styleUrls: ['./aside.component.css']
 })
-export class AsideComponent<T>{
+export class AsideComponent{
 
   @Input() title!: string;
-  @Input() items: T[] | undefined;
+  @Input() items: ITheme[] | undefined;
 
   constructor() { }
 
