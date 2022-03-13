2aed02c30162b9169fa21b72e75170b10c85b8da
chrisAXZA <nexter1@tutanota.com>
2022-03-13 16:09:56 +0200

    Implement environmet apiURL

--- Workshop-Forum/Forum-App/src/app/content.service.ts
+++ Workshop-Forum/Forum-App/src/app/content.service.ts
@@ -2,6 +2,10 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IPost, ITheme } from './shared/interfaces';
 
+import { environment } from '../environments/environment';
+
+const API_URL = environment.apiURL;
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,11 +14,13 @@ export class ContentService {
   constructor(private http: HttpClient) { }
 
   loadThemes() {
-    return this.http.get<ITheme[]>('http://localhost:3000/api/themes');
+    return this.http.get<ITheme[]>(`${API_URL}/themes`);
+    // return this.http.get<ITheme[]>('http://localhost:3000/api/themes');
   }
 
   loadPosts(take?: number) {
     const query = take ? `?limit=${take}` : '';
-    return this.http.get<IPost[]>(`http://localhost:3000/api/posts${query}`);
+    return this.http.get<IPost[]>(`${API_URL}/posts${query}`);
+    // return this.http.get<IPost[]>(`http://localhost:3000/api/posts${query}`);
   }
 }
