06a53caec4b01bd96ff30ced493a52f31b975f36
chrisAXZA <nexter1@tutanota.com>
2022-03-14 09:58:31 +0200

    Implement TemplateRef in aside-template

--- Workshop-Forum/Forum-App/src/app/core/aside/aside.component.ts
+++ Workshop-Forum/Forum-App/src/app/core/aside/aside.component.ts
@@ -1,15 +1,17 @@
-import { Component, Input } from '@angular/core';
-import { IPost } from 'src/app/shared/interfaces';
+import { Component, Input, TemplateRef } from '@angular/core';
+// import { IPost } from 'src/app/shared/interfaces';
 
 @Component({
   selector: 'app-aside',
   templateUrl: './aside.component.html',
   styleUrls: ['./aside.component.css']
 })
-export class AsideComponent{
+export class AsideComponent<T>{
 
   @Input() title!: string;
-  @Input() items: IPost[] | undefined;
+  @Input() items: T[] | undefined;
+  // @Input() items: IPost[] | undefined;
+  @Input() itemTemplateRef!: TemplateRef<{ $implicit: T }>;
 
   constructor() { }
 
