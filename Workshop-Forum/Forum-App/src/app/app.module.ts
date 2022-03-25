0eff6b23d809080449cc4b3c8cd7bbfe673f7c14
chrisAXZA <nexter1@tutanota.com>
2022-03-25 12:21:31 +0200

    Undo latest changes

--- Workshop-Forum/Forum-App/src/app/app.module.ts
+++ Workshop-Forum/Forum-App/src/app/app.module.ts
@@ -14,14 +14,14 @@ import { AppRoutingModule } from './app-routing.module';
 // 
 
 import { AppComponent } from './app.component';
-import { ContentService } from './content.service';
 import { CoreModule } from './core/core.module';
+import { ContentService } from './content.service';
 import { ThemeComponent } from './theme/theme.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ThemeComponent
+    ThemeComponent,
   ],
   imports: [
     BrowserModule,
