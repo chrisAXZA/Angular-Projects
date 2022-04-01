abc6e1c366a6a34464d1b439a037e9f82d4d3c06
chrisAXZA <nexter1@tutanota.com>
2022-04-01 16:33:37 +0300

    Implement dynamic route in ViewContact

--- HR-ContactForms/src/app/components/view-contact/view-contact.component.ts
+++ HR-ContactForms/src/app/components/view-contact/view-contact.component.ts
@@ -1,4 +1,7 @@
+import { ActivatedRoute, ParamMap, Params } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { ContactService } from 'src/app/services/contact.service';
+import { IContact } from 'src/app/models/IContact';
 
 @Component({
   selector: 'app-view-contact',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-contact.component.css']
 })
 export class ViewContactComponent implements OnInit {
+  public loading: boolean = false;
+  public contactId: string | null = null;
+  public contact: IContact = {} as IContact;
+  public errorMessage: string | null = null;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private contactService: ContactService) { }
 
   ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe({
+      next: (param: ParamMap) => { this.contactId = param.get('contactId') },
+    });
+
+    if (this.contactId) {
+      this.loading = true;
+      this.contactService.getContact(this.contactId).subscribe({
+        next: (data: IContact) => { this.contact = data; this.loading = false; },
+        error: (error) => { this.errorMessage = error; this.loading = false; },
+      });
+    }
+
   }
 
+  public isNotEmpty() {
+    return Object.keys(this.contact).length > 0;
+  }
 }
