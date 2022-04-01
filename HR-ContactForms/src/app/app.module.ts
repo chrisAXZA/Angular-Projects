7fc03a3a574bd56fd67b7d7ed63d18abcbf91860
chrisAXZA <nexter1@tutanota.com>
2022-04-01 12:04:13 +0300

    Implement contact service functions

--- HR-ContactForms/src/app/app.module.ts
+++ HR-ContactForms/src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { ContactManagerComponent } from './components/contact-manager/contact-manager.component';
-import { AddContactComponent } from './components/add-contact/add-contact.component';
-import { EditContactComponent } from './components/edit-contact/edit-contact.component';
-import { ViewContactComponent } from './components/view-contact/view-contact.component';
 import { SpinnerComponent } from './components/spinner/spinner.component';
+import { AddContactComponent } from './components/add-contact/add-contact.component';
+import { ViewContactComponent } from './components/view-contact/view-contact.component';
+import { EditContactComponent } from './components/edit-contact/edit-contact.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { ContactManagerComponent } from './components/contact-manager/contact-manager.component';
 
 
 @NgModule({
@@ -25,7 +26,8 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
