d1cf81f0d1863da69f7dc17284ac2e28f854c919
chrisAXZA <nexter1@tutanota.com>
2022-04-02 11:11:08 +0300

    Implement edit-contact form

--- HR-ContactForms/src/app/components/edit-contact/edit-contact.component.ts
+++ HR-ContactForms/src/app/components/edit-contact/edit-contact.component.ts
@@ -1,4 +1,9 @@
+import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+
+import { IContact } from 'src/app/models/IContact';
+import { IGroup } from 'src/app/models/IGroup';
+import { ContactService } from 'src/app/services/contact.service';
 
 @Component({
   selector: 'app-edit-contact',
@@ -7,9 +12,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditContactComponent implements OnInit {
 
-  constructor() { }
+  public loading: boolean = false;
+  public contactId: string | null = null;
+  public contact: IContact = {} as IContact;
+  public errorMessage: string | null = null;
+  public groups: IGroup[] = [] as IGroup[];
+  public group: IGroup = {} as IGroup;
+
+  constructor(private activatedRoute: ActivatedRoute, private contactService: ContactService) { }
 
   ngOnInit(): void {
+
+    this.loading = true;
+
+    this.activatedRoute.paramMap.subscribe({
+      next: (param: ParamMap) => { this.contactId = param.get('contactId') },
+    });
+
+    if (this.contactId) {
+      this.contactService.getContact(this.contactId).subscribe({
+        next: (data: IContact) => {
+          this.contact = data;
+          this.loading = false;
+          this.contactService.getAllGroups().subscribe({
+            next: (data: IGroup[]) => { this.groups = data; },
+          });
+          this.contactService.getGroup(this.contact).subscribe({
+            next: (data: IGroup) => { this.group = data; },
+          });
+        },
+        error: (error) => { this.errorMessage = error; this.loading = false; },
+      })
+    }
   }
 
 }
