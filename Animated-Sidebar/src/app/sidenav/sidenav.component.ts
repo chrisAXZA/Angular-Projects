9fdcd7e594a058cab7daf0fae750fefd82829517
chrisAXZA <nexter1@tutanota.com>
2022-04-19 10:14:25 +0300

    Setting up sidenav html template

--- Animated-Sidebar/src/app/sidenav/sidenav.component.ts
+++ Animated-Sidebar/src/app/sidenav/sidenav.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { navbarData } from './nav-data';
+
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss']
 })
 export class SidenavComponent implements OnInit {
+
+  collapsed = false;
+  navData = navbarData;
 
   constructor() { }
 
