4c6af5712edc1e0d8038bd518f69dfc6b565fa9c
chrisAXZA <nexter1@tutanota.com>
2022-04-19 11:24:29 +0300

    Implement sidenav toggle functionality

--- Animated-Sidebar/src/app/sidenav/sidenav.component.ts
+++ Animated-Sidebar/src/app/sidenav/sidenav.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 import { navbarData } from './nav-data';
+
+interface SideNavToggle {
+  screenWidth: number;
+  collapsed: boolean;
+}
 
 @Component({
   selector: 'app-sidenav',
@@ -9,12 +14,25 @@ import { navbarData } from './nav-data';
 })
 export class SidenavComponent implements OnInit {
 
-  collapsed = true;
+  @Output() onToggleSideNav: EventEmitter<SideNavToggle> = new EventEmitter();
+
+  collapsed = false;
+  screenWidth = 0;
   navData = navbarData;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public toggleCollapse(): void {
+    this.collapsed = !this.collapsed;
+    this.onToggleSideNav.emit({collapsed : this.collapsed, screenWidth: this.screenWidth});
+  }
+
+  public closeSidenav(): void {
+    this.collapsed = false;
+    this.onToggleSideNav.emit({collapsed : this.collapsed, screenWidth: this.screenWidth});
   }
 
 }
