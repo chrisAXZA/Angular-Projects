5f16d8d48396e6489c22014484584072e5339d79
chrisAXZA <nexter1@tutanota.com>
2022-04-19 12:11:44 +0300

    Update nav-data with new icons

--- Animated-Sidebar/src/app/sidenav/nav-data.ts
+++ Animated-Sidebar/src/app/sidenav/nav-data.ts
@@ -4,4 +4,34 @@ export const navbarData = [
         icon: 'fal fa-home',
         label: 'Dashboard',
     },
+    {
+        routeLink: 'products',
+        icon: 'fal fa-box-open',
+        label: 'Products',
+    },
+    {
+        routeLink: 'statistics',
+        icon: 'fal fa-chart-bar',
+        label: 'Statistics',
+    },
+    {
+        routeLink: 'coupens',
+        icon: 'fal fa-tags',
+        label: 'Coupens',
+    },
+    {
+        routeLink: 'pages',
+        icon: 'fal fa-file',
+        label: 'Pages',
+    },
+    {
+        routeLink: 'media',
+        icon: 'fal fa-camera',
+        label: 'Media',
+    },
+    {
+        routeLink: 'settings',
+        icon: 'fal fa-cog',
+        label: 'Settings',
+    },
 ];
