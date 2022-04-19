7edb9f17250f6d4ff83c75c183e2d0e78514812d
chrisAXZA <nexter1@tutanota.com>
2022-04-19 12:42:33 +0300

    Implement fadeInOut animation in sidenav

--- Animated-Sidebar/src/app/sidenav/sidenav.component.ts
+++ Animated-Sidebar/src/app/sidenav/sidenav.component.ts
@@ -1,3 +1,4 @@
+import { animate, animation, style, transition, trigger } from '@angular/animations';
 import { Component, EventEmitter, HostListener, OnInit, Output } from '@angular/core';
 
 import { navbarData } from './nav-data';
@@ -12,7 +13,20 @@ interface SideNavToggle {
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss'],
   animations: [
-    
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('350ms',
+          style({ opacity: 1 })
+        )
+      ]),
+      transition(':leave', [
+        style({ opacity: 1 }),
+        animate('350ms',
+          style({ opacity: 0 })
+        )
+      ]),
+    ])
   ]
 })
 export class SidenavComponent implements OnInit {
