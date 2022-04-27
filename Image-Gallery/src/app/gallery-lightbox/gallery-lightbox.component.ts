ede4df3246e659117fe436417dd1c12059d603ec
chrisAXZA <nexter1@tutanota.com>
2022-04-27 19:55:30 +0300

    Implement onPreviewImage functionality

--- Image-Gallery/src/app/gallery-lightbox/gallery-lightbox.component.ts
+++ Image-Gallery/src/app/gallery-lightbox/gallery-lightbox.component.ts
@@ -14,9 +14,18 @@ export class GalleryLightboxComponent implements OnInit {
 
   @Input() galleryData: Item[] = [];
 
+  previewImage = false;
+  showMask = false;
+  currentLightBoxImage: Item = this.galleryData[0];
+  currentIndex = 0;
+  controls = true;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public onPreviewImage(index: number): void {
+    // shows image 
+  }
 }
