686ce63c3b4b805ab83eaba395c08faf5c4ac31a
chrisAXZA <nexter1@tutanota.com>
2022-05-02 18:10:38 +0300

    Refactor to new code base

--- Reactive-Forms/src/app/app.module.ts
+++ Reactive-Forms/src/app/app.module.ts
@@ -11,22 +11,10 @@ import { MatChipsModule } from '@angular/material/chips';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ReactiveFormComponent } from './components/reactive-form/reactive-form.component';
-import { ArrayFormComponent } from './array-form/array-form.component';
-import { BasicFormComponent } from './basic-form/basic-form.component';
-import { NestedFormComponent } from './nested-form/nested-form.component';
-import { SubmitFormComponent } from './submit-form/submit-form.component';
-import { ValidFormComponent } from './valid-form/valid-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ReactiveFormComponent,
-    ArrayFormComponent,
-    BasicFormComponent,
-    NestedFormComponent,
-    SubmitFormComponent,
-    ValidFormComponent,
   ],
   imports: [
     BrowserModule,
