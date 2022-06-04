309ec3b2a80fb2a2cacc75174504e2d4c9bf3c40
chrisAXZA <nexter1@tutanota.com>
2022-06-04 16:13:10 +0300

    Generate login component

--- UserDB-Material/src/app/app.module.ts
+++ UserDB-Material/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
