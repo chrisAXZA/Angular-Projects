ccb582bc6a41979b631d12b6196e7d01605796a4
chrisAXZA <nexter1@tutanota.com>
2022-06-05 12:10:18 +0300

    Add dashboard component

--- UserDB-Material/src/app/components/dashboard/dashboard.module.ts
+++ UserDB-Material/src/app/components/dashboard/dashboard.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule } from '../shared/shared.module';
+import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DashboardComponent
+  ],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    SharedModule,
   ]
 })
 export class DashboardModule { }
