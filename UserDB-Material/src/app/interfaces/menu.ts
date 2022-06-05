c53cbb1a0d0e42215a0b1085e958eed91c1cf991
chrisAXZA <nexter1@tutanota.com>
2022-06-05 15:23:02 +0300

    Implement getMenu function

--- UserDB-Material/src/app/interfaces/menu.ts
+++ UserDB-Material/src/app/interfaces/menu.ts
@@ -1,4 +1,4 @@
-export interface Menu {
+export interface IMenu {
     name: string;
     redirect: string;
 }
--- UserDB-Material/src/app/services/menu.service.ts
+++ UserDB-Material/src/app/services/menu.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+
+import { IMenu } from '../interfaces/menu';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MenuService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getMenu(): Observable<IMenu[]> {
+   return this.http.get<IMenu[]>('./assets/data/menu.json');
+  }
 }
