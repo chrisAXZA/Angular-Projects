210e2b5c6c4228c06ae78d1aef034ad32ed9d9bc
chrisAXZA <nexter1@tutanota.com>
2022-06-05 20:15:18 +0300

    Add material table filter functionality

--- UserDB-Material/src/app/components/dashboard/users/users.component.ts
+++ UserDB-Material/src/app/components/dashboard/users/users.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 
 import { IUser } from 'src/app/interfaces/user';
 
-const userList: IUser[] = [
-  { username: 'pesho', firstName: 'Pesho', secondName: 'Peshov', gender: 'Male' },
-  { username: 'kotze', firstName: 'Kotze', secondName: 'Petrov', gender: 'Male' },
-  { username: 'maia', firstName: 'Maria', secondName: 'Ivana', gender: 'Female' },
-  { username: 'kesho', firstName: 'Fiodr', secondName: 'Antonov', gender: 'Male' },
-  { username: 'ana', firstName: 'Ana', secondName: 'Svetlana', gender: 'Female' },
-  { username: 'peter', firstName: 'Peter', secondName: 'Aleksov', gender: 'Male' },
-];
+// const userList: IUser[] = [
+//   { username: 'pesho', firstName: 'Pesho', secondName: 'Peshov', gender: 'Male' },
+//   { username: 'kotze', firstName: 'Kotze', secondName: 'Petrov', gender: 'Male' },
+//   { username: 'maia', firstName: 'Maria', secondName: 'Ivana', gender: 'Female' },
+//   { username: 'kesho', firstName: 'Fiodr', secondName: 'Antonov', gender: 'Male' },
+//   { username: 'ana', firstName: 'Ana', secondName: 'Svetlana', gender: 'Female' },
+//   { username: 'peter', firstName: 'Peter', secondName: 'Aleksov', gender: 'Male' },
+// ];
 
 @Component({
   selector: 'app-users',
@@ -18,12 +19,27 @@ const userList: IUser[] = [
 })
 export class UsersComponent implements OnInit {
 
+  userList: IUser[] = [
+    { username: 'pesho', firstName: 'Pesho', secondName: 'Peshov', gender: 'Male' },
+    { username: 'kotze', firstName: 'Kotze', secondName: 'Petrov', gender: 'Male' },
+    { username: 'maia', firstName: 'Maria', secondName: 'Ivana', gender: 'Female' },
+    { username: 'kesho', firstName: 'Fiodr', secondName: 'Antonov', gender: 'Male' },
+    { username: 'ana', firstName: 'Ana', secondName: 'Svetlana', gender: 'Female' },
+    { username: 'peter', firstName: 'Peter', secondName: 'Aleksov', gender: 'Male' },
+  ];
+
   displayedColumns: string[] = ['username', 'firstName', 'secondName', 'gender', 'actions'];
-  dataSource = userList;
+  // dataSource = userList;
+  dataSource = new MatTableDataSource(this.userList);
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
