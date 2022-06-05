a5fa97a7b55c6de1fc0bbc152451343b058f5204
chrisAXZA <nexter1@tutanota.com>
2022-06-05 18:09:10 +0300

    Add edit icons to table

--- UserDB-Material/src/app/components/dashboard/users/users.component.ts
+++ UserDB-Material/src/app/components/dashboard/users/users.component.ts
@@ -3,12 +3,12 @@ import { Component, OnInit } from '@angular/core';
 import { IUser } from 'src/app/interfaces/user';
 
 const userList: IUser[] = [
-  {username: 'pesho', firstName: 'Pesho', secondName: 'Peshov', gender: 'Male'},
-  {username: 'kotze', firstName: 'Kotze', secondName: 'Petrov', gender: 'Male'},
-  {username: 'maia', firstName: 'Maria', secondName: 'Ivana', gender: 'Female'},
-  {username: 'kesho', firstName: 'Fiodr', secondName: 'Antonov', gender: 'Male'},
-  {username: 'ana', firstName: 'Ana', secondName: 'Svetlana', gender: 'Female'},
-  {username: 'peter', firstName: 'Peter', secondName: 'Aleksov', gender: 'Male'},
+  { username: 'pesho', firstName: 'Pesho', secondName: 'Peshov', gender: 'Male' },
+  { username: 'kotze', firstName: 'Kotze', secondName: 'Petrov', gender: 'Male' },
+  { username: 'maia', firstName: 'Maria', secondName: 'Ivana', gender: 'Female' },
+  { username: 'kesho', firstName: 'Fiodr', secondName: 'Antonov', gender: 'Male' },
+  { username: 'ana', firstName: 'Ana', secondName: 'Svetlana', gender: 'Female' },
+  { username: 'peter', firstName: 'Peter', secondName: 'Aleksov', gender: 'Male' },
 ];
 
 @Component({
@@ -18,7 +18,7 @@ const userList: IUser[] = [
 })
 export class UsersComponent implements OnInit {
 
-  displayedColumns: string[] = ['username', 'firstName', 'secondName', 'gender'];
+  displayedColumns: string[] = ['username', 'firstName', 'secondName', 'gender', 'actions'];
   dataSource = userList;
 
   constructor() { }
