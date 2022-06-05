f7db55fd1c58867ab7800057cafdb55adbb68ff1
chrisAXZA <nexter1@tutanota.com>
2022-06-05 13:40:27 +0300

    Implement routerLinks with navbar buttons

--- UserDB-Material/src/app/components/dashboard/dashboard-routing.module.ts
+++ UserDB-Material/src/app/components/dashboard/dashboard-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
+import { InitialComponent } from './initial/initial.component';
+import { ReportsComponent } from './reports/reports.component';
+import { UsersComponent } from './users/users.component';
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent, pathMatch: 'full', },
+  {
+    path: '', component: DashboardComponent, children: [
+      { path: '', component: InitialComponent, pathMatch: 'full', },
+      { path: 'users', component: UsersComponent, },
+      { path: 'reports', component: ReportsComponent, },
+    ],
+  },
 ];
 
 @NgModule({
