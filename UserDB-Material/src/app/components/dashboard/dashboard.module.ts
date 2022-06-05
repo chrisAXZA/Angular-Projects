bb14ebaf1427f2cce00a5fea8b867bde9fc05855
chrisAXZA <nexter1@tutanota.com>
2022-06-05 12:56:44 +0300

    Add initial+navbar+reports+users components

--- UserDB-Material/src/app/components/dashboard/dashboard.module.ts
+++ UserDB-Material/src/app/components/dashboard/dashboard.module.ts
@@ -3,12 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { SharedModule } from '../shared/shared.module';
 import { DashboardComponent } from './dashboard.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { InitialComponent } from './initial/initial.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { UsersComponent } from './users/users.component';
+import { ReportsComponent } from './reports/reports.component';
 
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    InitialComponent,
+    NavbarComponent,
+    UsersComponent,
+    ReportsComponent
   ],
   imports: [
     CommonModule,
