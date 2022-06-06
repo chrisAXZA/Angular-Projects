e4593286d5f717e1bc38dd10310e2809d8912e38
chrisAXZA <nexter1@tutanota.com>
2022-06-06 18:53:14 +0300

    Add snackBar to addUser

--- UserDB-Material/src/app/components/dashboard/users/create-user/create-user.component.ts
+++ UserDB-Material/src/app/components/dashboard/users/create-user/create-user.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
 import { IUser } from 'src/app/interfaces/user';
 import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-create-user',
@@ -18,7 +19,8 @@ export class CreateUserComponent implements OnInit {
   constructor(
     private fb: FormBuilder,
     private router: Router,
-    private _userService: UserService) {
+    private _userService: UserService,
+    private snackBar: MatSnackBar) {
     this.form = this.fb.group({
       username: ['', Validators.required],
       firstName: ['', Validators.required],
@@ -36,6 +38,12 @@ export class CreateUserComponent implements OnInit {
     const { username, firstName, secondName, gender } = this.form.value;
     this._userService.addUser({ username, firstName, secondName, gender } as IUser);
     this.router.navigate(['/dashboard/users']);
+
+    this.snackBar.open(`User ${username} has been added!`, '', {
+      duration: 1500,
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+    });
   }
 
 }
