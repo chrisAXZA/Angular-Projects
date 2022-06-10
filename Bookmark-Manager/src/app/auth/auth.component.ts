63181cd185c21d85e1580a4e08290af6f13b1882
chrisAXZA <nexter1@tutanota.com>
2022-06-10 21:32:05 +0300

    Implement auth component/template

--- Bookmark-Manager/src/app/auth/auth.component.ts
+++ Bookmark-Manager/src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthComponent implements OnInit {
 
+  @Output() onSubmitEvent = new EventEmitter<any>();
+  @Input() submitLabel: string;
+
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', Validators.required);
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  getEmailErrorMessage(): string {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value!';
+    }
+
+    return this.email.hasError('email')
+      ? 'Not a valid email!'
+      : '';
+  }
+
+  getPasswordErrorMessage(): string {
+    if (this.password.hasError('required')) {
+      return 'You must enter a value!';
+    }
+
+    return '';
+  }
+
+  onSubmit(): void {
+    this.onSubmitEvent.emit({
+      email: this.email.value,
+      password: this.password.value,
+    });
   }
 
 }
