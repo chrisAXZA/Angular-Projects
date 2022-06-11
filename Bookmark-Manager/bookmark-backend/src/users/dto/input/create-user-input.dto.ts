abc1af37ffe102edbaa30aed2f80a25e76437170
chrisAXZA <nexter1@tutanota.com>
2022-06-11 16:22:19 +0300

    Add class-validators to create-user dto

--- Bookmark-Manager/bookmark-backend/src/users/dto/input/create-user-input.dto.ts
+++ Bookmark-Manager/bookmark-backend/src/users/dto/input/create-user-input.dto.ts
@@ -1,10 +1,14 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator'
 import { Field, InputType } from "@nestjs/graphql";
 
 @InputType()
-export class CreateUserInput{
+export class CreateUserInput {
     @Field()
+    @IsEmail()
     readonly email: string;
-    
+
     @Field()
+    @IsNotEmpty()
+    @IsString()
     readonly password: string;
 }
