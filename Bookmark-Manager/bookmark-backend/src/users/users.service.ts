03cb3bf9b839845d556a1607120ee7aae6dacb22
chrisAXZA <nexter1@tutanota.com>
2022-06-11 16:29:35 +0300

    Add get-user.args dto

--- Bookmark-Manager/bookmark-backend/src/users/users.service.ts
+++ Bookmark-Manager/bookmark-backend/src/users/users.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserInput } from './dto/input/create-user-input.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    async createUser(createUserData: CreateUserInput){
+        
+    }
+}
