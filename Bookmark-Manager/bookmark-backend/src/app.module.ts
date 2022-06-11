702be07483db6be98e985de8a8e8173c0942c8fb
chrisAXZA <nexter1@tutanota.com>
2022-06-11 16:53:00 +0300

    Add nestJS configModule in app.module

--- Bookmark-Manager/bookmark-backend/src/app.module.ts
+++ Bookmark-Manager/bookmark-backend/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config'
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 
@@ -8,6 +9,11 @@ import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      // declares ConfigModule as global so no need to 
+      // redeclare in each module where being used
+    }),
     // graphql schema file will be automatically generated, code-first approach + file in memory
     // driver: constitutes serverAdapter in this case Apollo
     GraphQLModule.forRoot<ApolloDriverConfig>({ autoSchemaFile: true, driver: ApolloDriver }),
