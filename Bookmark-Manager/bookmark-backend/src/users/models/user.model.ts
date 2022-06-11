e4f9e77e95af6e9bca386ee1e3d50407acca3781
chrisAXZA <nexter1@tutanota.com>
2022-06-11 14:25:49 +0300

    Set up user model

--- Bookmark-Manager/bookmark-backend/src/users/models/user.model.ts
+++ Bookmark-Manager/bookmark-backend/src/users/models/user.model.ts
@@ -1,6 +1,9 @@
-import { ObjectType } from "@nestjs/graphql";
+import { Field, ObjectType } from "@nestjs/graphql";
+
+import { AbstractModel } from "src/common/abstract.model";
 
 @ObjectType()
-export class User {
-
+export class User extends AbstractModel {
+    @Field()
+    readonly email: string;
 }
