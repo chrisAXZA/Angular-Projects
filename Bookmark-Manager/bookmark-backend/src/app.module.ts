7e4fee5e4108ca1d245f01b6b6245f1f99f0083d
chrisAXZA <nexter1@tutanota.com>
2022-06-11 17:59:52 +0300

    Add joi validation to app.module

--- Bookmark-Manager/bookmark-backend/src/app.module.ts
+++ Bookmark-Manager/bookmark-backend/src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config'
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
+// import { Joi } from 'joi';
+import Joi = require("joi")
 
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
@@ -13,6 +15,12 @@ import { UsersModule } from './users/users.module';
       isGlobal: true,
       // declares ConfigModule as global so no need to 
       // redeclare in each module where being used
+      validationSchema: Joi.object({
+        PORT: Joi.number().required,
+        MONGO_URI: Joi.string().required,
+        // when app starts up and no PORT or MONGO_URI is
+        // provided, Joi will inform the user with notification
+      }),
     }),
     // graphql schema file will be automatically generated, code-first approach + file in memory
     // driver: constitutes serverAdapter in this case Apollo
