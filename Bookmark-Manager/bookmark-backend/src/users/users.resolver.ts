d91f937e3f040303145c7237243a60ded87d79c0
chrisAXZA <nexter1@tutanota.com>
2022-06-11 16:23:21 +0300

    Implement users.resolver

--- Bookmark-Manager/bookmark-backend/src/users/users.resolver.ts
+++ Bookmark-Manager/bookmark-backend/src/users/users.resolver.ts
@@ -1,4 +1,5 @@
 import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { CreateUserInput } from './dto/input/create-user-input.dto';
 
 import { User } from './models/user.model';
 import { UsersService } from './users.service';
@@ -8,5 +9,7 @@ export class UsersResolver {
     constructor(private readonly userService: UsersService) { }
 
     @Mutation(() => User)
-    async createUser(@Args('createUserData') createUserData)
+    async createUser(@Args('createUserData') createUserData: CreateUserInput){
+        return this.userService.createUser();
+    }
 }
