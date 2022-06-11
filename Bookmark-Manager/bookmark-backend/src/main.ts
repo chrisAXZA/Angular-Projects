42404e7031385c66afbd8c53bbdf2922878dc294
chrisAXZA <nexter1@tutanota.com>
2022-06-11 16:58:32 +0300

    Add configService in main.ts

--- Bookmark-Manager/bookmark-backend/src/main.ts
+++ Bookmark-Manager/bookmark-backend/src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app.module';
@@ -6,6 +7,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<string>('PORT');
+
+  await app.listen(port);
 }
 bootstrap();
