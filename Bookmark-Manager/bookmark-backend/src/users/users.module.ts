ebbfeefd85c5a8ca9d5ed3682b40abd77529e178
chrisAXZA <nexter1@tutanota.com>
2022-06-17 16:51:10 +0300

    Import mongoose module in users module

--- Bookmark-Manager/bookmark-backend/src/users/users.module.ts
+++ Bookmark-Manager/bookmark-backend/src/users/users.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
-import { UsersResolver } from './users.resolver';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { User } from './models/user.model';
 import { UsersService } from './users.service';
+import { UsersResolver } from './users.resolver';
+import { UserSchema } from './models/user.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+    ]),
+  ],
   providers: [UsersResolver, UsersService]
 })
-export class UsersModule {}
+export class UsersModule { }
