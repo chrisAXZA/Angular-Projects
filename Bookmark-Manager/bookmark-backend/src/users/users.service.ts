d502811f611c5203db53b703f9b4725058253bb0
chrisAXZA <nexter1@tutanota.com>
2022-06-17 19:39:51 +0300

    Add bcrypt package

--- Bookmark-Manager/bookmark-backend/src/users/users.service.ts
+++ Bookmark-Manager/bookmark-backend/src/users/users.service.ts
@@ -1,15 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
 
+import { UsersRepository } from './users.repository';
 import { GetUserArgs } from './dto/args/get-user-args.dto';
 import { CreateUserInput } from './dto/input/create-user-input.dto';
 
 @Injectable()
 export class UsersService {
-    async createUser(createUserData: CreateUserInput){
+
+    constructor(private readonly usersRepository: UsersRepository) { }
+
+    async createUser(createUserData: CreateUserInput) {
+        await this.validateCreateUserData(createUserData);
+
+        // password needs to be stored in hashed format
 
     }
-    
-    async getUser(getUserArgs: GetUserArgs){
 
+    async getUser(getUserArgs: GetUserArgs) {
+
+    }
+
+    private async validateCreateUserData(createUserData: CreateUserInput) {
+        try {
+            await this.usersRepository.findOne({ email: createUserData.email })
+            throw new UnprocessableEntityException('Email is already being used!');
+        } catch (error) { }
     }
 }
