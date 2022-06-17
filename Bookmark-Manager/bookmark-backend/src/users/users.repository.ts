44e98b9cbe8038548f83522293b6e8b6c8b7cb37
chrisAXZA <nexter1@tutanota.com>
2022-06-17 19:16:54 +0300

    Implement users repository

--- Bookmark-Manager/bookmark-backend/src/users/users.repository.ts
+++ Bookmark-Manager/bookmark-backend/src/users/users.repository.ts
@@ -1,7 +1,18 @@
-import { Injectable } from "@nestjs/common";
+import { Model } from "mongoose";
+import { InjectModel } from "@nestjs/mongoose";
+import { Injectable, Logger } from "@nestjs/common";
+
+import { User } from "./models/user.model";
+import { UserDocument } from "./models/user.schema";
+import { AbstractRepository } from "src/database/abstract.repository";
 
 @Injectable()
-export class UsersRepository{
+export class UsersRepository extends AbstractRepository<UserDocument>{
     // functions allow to create or find user in DB
-    
+    protected readonly logger = new Logger(UsersRepository.name);
+
+    constructor(@InjectModel(User.name) userModel: Model<UserDocument>) {
+        super(userModel);
+    }
+
 }
