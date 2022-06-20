c94920608eb110f44802f384b5e0ea7db3aacc3d
chrisAXZA <nexter1@tutanota.com>
2022-06-20 17:32:29 +0300

    Add cookieParser in main.ts

--- Bookmark-Manager/bookmark-backend/src/main.ts
+++ Bookmark-Manager/bookmark-backend/src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 
@@ -6,12 +7,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // add cookieParser as global middleware
+  app.use(cookieParser());
+  
   // Any request send to the server will go through the ValidationPipe
   // which will handle any class validators in the model classes (@IsEmail, @IsString)
   app.useGlobalPipes(new ValidationPipe());
 
   // const configService = app.get(ConfigService);
   // const port = configService.get<string>('PORT');
+
+
 
   // await app.listen(port);
   await app.listen(3000);
