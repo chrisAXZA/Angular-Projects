bed5391847296dc928ba23712a5445d9cd154b75
chrisAXZA <nexter1@tutanota.com>
2022-06-20 14:27:10 +0300

    Implement auth.controller login

--- Bookmark-Manager/bookmark-backend/src/auth/auth.controller.ts
+++ Bookmark-Manager/bookmark-backend/src/auth/auth.controller.ts
@@ -1,5 +1,9 @@
-import { Controller, Post, UseGuards } from '@nestjs/common';
+import { Response } from 'express';
+import { Controller, Post, Res, UseGuards } from '@nestjs/common';
 
+import { AuthService } from './auth.service';
+import { User } from 'src/users/models/user.model';
+import { CurrentUser } from './current-user.decorator';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 
 // app will use HttpOnly cookies as authentication mechanism
@@ -7,11 +11,17 @@ import { LocalAuthGuard } from './guards/local-auth.guard';
 // This is the most secure way to store jwt
 @Controller('auth')
 export class AuthController {
+    constructor(private readonly authService: AuthService) { }
 
     @UseGuards(LocalAuthGuard) // triggers guard whenever this route is run => localAuthGuard will run LocalStrategy
     @Post('login')
     // gets current User of ongoing request, allows access to userObject from ongoing request
-    async login(
+    // Also requires response object in order to set cookie on the current response
+    async login(@CurrentUser() user: User,
+        @Res({ passthrough: true }) response: Response) { // sends to the normal NestJS response processing
+        await this.authService.login(user, response);
 
-    )
+        response.send(user);
+    }
+
 }
