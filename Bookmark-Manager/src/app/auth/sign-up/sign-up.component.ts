6ed1b5814c6850f8ca503d10cfe7fcead8171b1a
chrisAXZA <nexter1@tutanota.com>
2022-06-21 18:03:52 +0300

    Add jwt when user has successfully signed up

--- Bookmark-Manager/src/app/auth/sign-up/sign-up.component.ts
+++ Bookmark-Manager/src/app/auth/sign-up/sign-up.component.ts
@@ -1,8 +1,9 @@
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
-import { CreateUserGQL } from '../../../generated-types';
-// import { CreateUserGQL } from 'src/generated-types';
+import { LoginService } from '../login/login.service';
+import { CreateUserGQL, CreateUserInput } from '../../../generated-types';
+import { concatMap } from 'rxjs';
 
 @Component({
   selector: 'app-sign-up',
@@ -11,17 +12,33 @@ import { CreateUserGQL } from '../../../generated-types';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor(private readonly createUserGql: CreateUserGQL, private readonly router: Router) { }
+  constructor(
+    private readonly createUserGql: CreateUserGQL,
+    private readonly loginService: LoginService,
+    private readonly router: Router) { }
 
   ngOnInit(): void {
   }
 
-  signUp({ email, password }: any): void {
+  // signUp({ email, password }: any): void {
+  // Modify signUp so that it creates new auth jwt
+  // after has successfully signed in
+  signUp(createUserData: CreateUserInput) {
     // console.log(email, password);
 
     // mutate requires subscribe for call to be made
     this.createUserGql
-      .mutate({ createUserData: { email, password } })
+      // .mutate({ createUserData: { email, password } })
+      .mutate({ createUserData })
+      .pipe(
+        // combineMap will combine two observables => 
+        // first create the user then execute the next observable
+        concatMap(() => {
+          // loginService will ensure that user will have valid jwt
+          // when directed to homePage route
+          return this.loginService.login(createUserData);
+        }),
+      )
       .subscribe(() => {
         this.router.navigate(['/']);
       });
