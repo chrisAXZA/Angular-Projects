fb937721a8283b6561af1e4a9f1d80adc4d18085
chrisAXZA <nexter1@tutanota.com>
2022-06-21 12:09:29 +0300

    Add login.service at fron-end + login

---
+++ Bookmark-Manager/src/app/auth/login/login.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { CreateUserInput, User } from "src/generated-types";
+
+@Injectable({
+    // root, so that service can be used throughout the whole app
+    providedIn: 'root',
+})
+export class LoginService {
+    // httpClient used to send request to backend
+    constructor(private readonly httpClient: HttpClient) { }
+
+    // loginReques takes in email + password
+    login(loginRequest: CreateUserInput) {
+        // at runtime will be converted to actual api route
+        // returns User
+        return this.httpClient.post<User>('api/auth/login', loginRequest);
+    }
+}
