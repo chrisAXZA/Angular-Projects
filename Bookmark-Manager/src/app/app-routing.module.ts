e8de6894dd086008edc1abf83743325513898aa5
chrisAXZA <nexter1@tutanota.com>
2022-06-21 12:34:37 +0300

    Add home route to app.routing

--- Bookmark-Manager/src/app/app-routing.module.ts
+++ Bookmark-Manager/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './auth/login/login.component';
 import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent, },
   { path: 'signup', component: SignUpComponent, },
+  { path: '', component: HomeComponent, pathMatch: 'full', },
 ];
 
 @NgModule({
