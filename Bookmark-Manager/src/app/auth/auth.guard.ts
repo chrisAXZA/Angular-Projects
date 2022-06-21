ea97f477a3a3eda28883de3a2d273a6e8de8cb39
chrisAXZA <nexter1@tutanota.com>
2022-06-21 15:12:45 +0300

    Implement auth.guard canActivate

--- Bookmark-Manager/src/app/auth/auth.guard.ts
+++ Bookmark-Manager/src/app/auth/auth.guard.ts
@@ -1,11 +1,27 @@
 import { Injectable } from "@angular/core";
-import { CanActivate } from "@angular/router";
+import { CanActivate, Router } from "@angular/router";
+
+import { tap } from "rxjs";
+
+import { AuthService } from "./auth.service";
 
 @Injectable({
     providedIn: 'root',
 })
 // special angular function, which validates whether given user can access given route
 // Will be implemented abstractly so that can be used in various components
-export class AuthGuard implements CanActivate{ 
+export class AuthGuard implements CanActivate {
+    constructor(private readonly authService: AuthService, private readonly router: Router) { }
 
+    canActivate() {
+        return this.authService.isAuthenticated()
+            .pipe(
+                // authenticated is either true/false
+                tap((authenticated) => {
+                    if (!authenticated) {
+                        this.router.navigate(['/login']);
+                    }
+                }),
+            );
+    }
 }
