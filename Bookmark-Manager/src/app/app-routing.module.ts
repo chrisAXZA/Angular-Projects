871dafa01e4f55e3954747d7d10dcb5da91f9bb2
chrisAXZA <nexter1@tutanota.com>
2022-06-21 15:36:55 +0300

    Add authGuard to home route

--- Bookmark-Manager/src/app/app-routing.module.ts
+++ Bookmark-Manager/src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AuthGuard } from './auth/auth.guard';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './auth/login/login.component';
 import { SignUpComponent } from './auth/sign-up/sign-up.component';
-import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent, },
   { path: 'signup', component: SignUpComponent, },
-  { path: '', component: HomeComponent, pathMatch: 'full', },
+  { path: '', component: HomeComponent, pathMatch: 'full', canActivate: [AuthGuard], },
 ];
 
 @NgModule({
