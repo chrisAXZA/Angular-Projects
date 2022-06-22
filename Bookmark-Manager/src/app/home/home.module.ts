b30fc6058f8232a4a94b1fdb238891241b2990c2
chrisAXZA <nexter1@tutanota.com>
2022-06-22 12:55:16 +0300

    Import BookmarksModule in home.module

--- Bookmark-Manager/src/app/home/home.module.ts
+++ Bookmark-Manager/src/app/home/home.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeComponent } from './home.component';
+import { BookmarksModule } from '../bookmarks/bookmarks.module';
 
 @NgModule({
   declarations: [
     HomeComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    BookmarksModule,
   ]
 })
 export class HomeModule { }
