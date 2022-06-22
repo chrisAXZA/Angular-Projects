f877315723960720d254908f8633c4318c53af60
chrisAXZA <nexter1@tutanota.com>
2022-06-22 11:46:46 +0300

    Add BookmarksRepository + Bookmark model in module

--- Bookmark-Manager/bookmark-backend/src/bookmarks/bookmarks.module.ts
+++ Bookmark-Manager/bookmark-backend/src/bookmarks/bookmarks.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
+import { Bookmark } from './models/bookmark.model';
 import { BookmarksService } from './bookmarks.service';
 import { BookmarksResolver } from './bookmarks.resolver';
+import { BookmarkSchema } from './models/bookmark.schema';
+import { BookmarksRepository } from './bookmarks.repository';
 
 @Module({
-  providers: [BookmarksResolver, BookmarksService]
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Bookmark.name,
+        schema: BookmarkSchema,
+      },
+    ]),
+  ],
+  providers: [
+    BookmarksResolver,
+    BookmarksService,
+    BookmarksRepository,
+  ]
 })
-export class BookmarksModule {}
+export class BookmarksModule { }
