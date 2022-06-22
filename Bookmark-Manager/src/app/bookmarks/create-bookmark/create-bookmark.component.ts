6d09c3b5314fa0337efa4a120530329e6c79bc70
chrisAXZA <nexter1@tutanota.com>
2022-06-22 15:01:19 +0300

    Implement getBookmarkNameError in create-bookmark

--- Bookmark-Manager/src/app/bookmarks/create-bookmark/create-bookmark.component.ts
+++ Bookmark-Manager/src/app/bookmarks/create-bookmark/create-bookmark.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-create-bookmark',
-  templateUrl: './create-bookmark.component.html',
-  styleUrls: ['./create-bookmark.component.scss']
+    selector: 'app-create-bookmark',
+    templateUrl: './create-bookmark.component.html',
+    styleUrls: ['./create-bookmark.component.scss']
 })
 export class CreateBookmarkComponent implements OnInit {
 
-  constructor() { }
+    bookmarkName = new FormControl('', [Validators.required]);
 
-  ngOnInit(): void {
-  }
+    constructor() { }
 
+    ngOnInit(): void {
+    }
+
+    getBookmarkNameError() {
+        if (this.bookmarkName.hasError('required')) {
+            return 'You must enter a value!';
+        }
+
+        return '';
+    }
+
+    createBookmark() {
+
+    }
 }
