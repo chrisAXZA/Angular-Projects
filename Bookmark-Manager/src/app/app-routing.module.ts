4c2d8faecf5a80f2f901bcfe0a1f9ba793fe59f1
chrisAXZA <nexter1@tutanota.com>
2022-06-22 21:12:37 +0300

    Add bookmark route to app.routing

--- Bookmark-Manager/src/app/app-routing.module.ts
+++ Bookmark-Manager/src/app/app-routing.module.ts
@@ -5,8 +5,10 @@ import { AuthGuard } from './auth/auth.guard';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './auth/login/login.component';
 import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { BookmarkComponent } from './bookmarks/bookmark/bookmark.component';
 
 const routes: Routes = [
+  { path: 'bookmarks/:id', component: BookmarkComponent,canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent, },
   { path: 'signup', component: SignUpComponent, },
   { path: '', component: HomeComponent, pathMatch: 'full', canActivate: [AuthGuard], },
