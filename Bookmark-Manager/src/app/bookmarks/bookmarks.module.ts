a4df5607644b73f3c429612ee94bc67d61ad067f
chrisAXZA <nexter1@tutanota.com>
2022-06-22 12:57:17 +0300

    Move CreateBookmarkModule into bookmarks.module

--- Bookmark-Manager/src/app/bookmarks/bookmarks.module.ts
+++ Bookmark-Manager/src/app/bookmarks/bookmarks.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { BookmarksComponent } from './bookmarks.component';
+import { CreateBookmarkModule } from './create-bookmark/create-bookmark.module';
 
 @NgModule({
   declarations: [
     BookmarksComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    CreateBookmarkModule,
   ],
   exports: [
     BookmarksComponent,
