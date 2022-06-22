d8c1d5f7f8b72aa56a76b1e1008db1e3250a4f75
chrisAXZA <nexter1@tutanota.com>
2022-06-22 12:20:12 +0300

    Implement createBookmark in bookmarks.service

--- Bookmark-Manager/bookmark-backend/src/bookmarks/bookmarks.repository.ts
+++ Bookmark-Manager/bookmark-backend/src/bookmarks/bookmarks.repository.ts
@@ -2,9 +2,9 @@ import { Model } from "mongoose";
 import { InjectModel } from "@nestjs/mongoose";
 import { Injectable, Logger } from "@nestjs/common";
 
+import { Bookmark } from "./models/bookmark.model";
 import { BookmarkDocument } from "./models/bookmark.schema";
 import { AbstractRepository } from "src/database/abstract.repository";
-import { Bookmark } from "./models/bookmark.model";
 
 @Injectable()
 export class BookmarksRepository extends AbstractRepository<BookmarkDocument>{
--- Bookmark-Manager/bookmark-backend/src/bookmarks/bookmarks.service.ts
+++ Bookmark-Manager/bookmark-backend/src/bookmarks/bookmarks.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
 
+import { BookmarksRepository } from './bookmarks.repository';
+import { CreateBookmarkInput } from './dto/create-bookmark-input.dto';
+import { BookmarkDocument } from './models/bookmark.schema';
+
 @Injectable()
-export class BookmarksService {}
+export class BookmarksService {
+
+    constructor(private readonly bookmarksRepository: BookmarksRepository) { }
+
+    async createBookmark(createBookmarkData: CreateBookmarkInput, userId: string) {
+        const bookmarkDocument = await this.bookmarksRepository.create({
+            ...createBookmarkData,
+            links: [],
+            userId,
+        });
+
+        return this.toModel(bookmarkDocument);
+    }
+
+    // will return same object with new _id
+    private toModel(bookmarkDocument: BookmarkDocument) {
+        return {
+            _id: bookmarkDocument._id.toHexString(),
+            ...bookmarkDocument,
+        };
+    }
+}
