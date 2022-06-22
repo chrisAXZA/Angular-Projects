307b688631ec2c40830ef4a6f10f6c1ec2918e4a
chrisAXZA <nexter1@tutanota.com>
2022-06-22 20:11:56 +0300

    Add bookmarks observable in bookmarks component

--- Bookmark-Manager/src/app/bookmarks/bookmarks.component.ts
+++ Bookmark-Manager/src/app/bookmarks/bookmarks.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { map } from 'rxjs';
 
+import { Observable } from 'rxjs';
+
+import { Bookmark, BookmarksGQL } from 'src/generated-types';
 import { CreateBookmarkComponent } from './create-bookmark/create-bookmark.component';
 
 @Component({
@@ -10,9 +14,16 @@ import { CreateBookmarkComponent } from './create-bookmark/create-bookmark.compo
 })
 export class BookmarksComponent implements OnInit {
 
-    constructor(private readonly dialog: MatDialog) { }
+    bookmarks$: Observable<Bookmark[]>;
+
+    constructor(private readonly dialog: MatDialog, private readonly bookmarkGql: BookmarksGQL) { }
 
     ngOnInit(): void {
+        // watches changes, wether from direct call or cache
+        this.bookmarks$ = this.bookmarkGql.watch().valueChanges
+            .pipe(
+                map((result) => result.data.bookmarks),
+            );
     }
 
     onFabClick() {
