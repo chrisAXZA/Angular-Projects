c930a50b5921ad5dfc453254649deab7eb2725fe
chrisAXZA <nexter1@tutanota.com>
2022-06-22 12:37:24 +0300

    Add bookmarks module + component

---
+++ Bookmark-Manager/src/app/bookmarks/bookmarks.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BookmarksComponent } from './bookmarks.component';
+
+
+
+@NgModule({
+  declarations: [
+    BookmarksComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class BookmarksModule { }
