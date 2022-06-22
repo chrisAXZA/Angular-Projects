974aa52a01941e1cde8ae915067c07fd24e40dd1
chrisAXZA <nexter1@tutanota.com>
2022-06-22 14:44:58 +0300

    Import reactiveForm/Material modules create.module

--- Bookmark-Manager/src/app/bookmarks/create-bookmark/create-bookmark.module.ts
+++ Bookmark-Manager/src/app/bookmarks/create-bookmark/create-bookmark.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 import { CreateBookmarkComponent } from './create-bookmark.component';
-
-
 
 @NgModule({
   declarations: [
     CreateBookmarkComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
   ]
 })
 export class CreateBookmarkModule { }
