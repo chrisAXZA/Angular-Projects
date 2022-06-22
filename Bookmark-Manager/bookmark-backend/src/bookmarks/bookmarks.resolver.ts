650c977a0799b354118403c93011b46c26885c25
chrisAXZA <nexter1@tutanota.com>
2022-06-22 12:08:29 +0300

    Implement bookmark-input dto

--- Bookmark-Manager/bookmark-backend/src/bookmarks/bookmarks.resolver.ts
+++ Bookmark-Manager/bookmark-backend/src/bookmarks/bookmarks.resolver.ts
@@ -1,4 +1,17 @@
-import { Resolver } from '@nestjs/graphql';
+import { Mutation, Resolver } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
 
-@Resolver()
-export class BookmarksResolver {}
+import { Bookmark } from './models/bookmark.model';
+import { BookmarksService } from './bookmarks.service';
+import { GqlAuthGuard } from 'src/auth/guards/gql-auth.guard';
+
+// resolver will actually create new bookmarks entities
+@Resolver(() => Bookmark) // will return a type of bookmark
+export class BookmarksResolver {
+    constructor(private readonly bookmarksService: BookmarksService) { }
+
+    // only authenticated users should be allowed to create new Bookmarks
+    @UseGuards(GqlAuthGuard)
+    @Mutation(() => Bookmark) // will return new Bookmark
+    async createBookmark() { }
+}
