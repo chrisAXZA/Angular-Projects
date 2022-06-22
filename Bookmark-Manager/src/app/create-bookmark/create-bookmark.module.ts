70a167bbb50ef8caf15687ddfddf2e776eedbf48
chrisAXZA <nexter1@tutanota.com>
2022-06-22 12:38:45 +0300

    Add create-Bookmark module + component

---
+++ Bookmark-Manager/src/app/create-bookmark/create-bookmark.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CreateBookmarkComponent } from './create-bookmark.component';
+
+
+
+@NgModule({
+  declarations: [
+    CreateBookmarkComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CreateBookmarkModule { }
