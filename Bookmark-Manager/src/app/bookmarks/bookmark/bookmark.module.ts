af3c4a91a5eb6227ce478c2ac19eb1652e4dbd01
chrisAXZA <nexter1@tutanota.com>
2022-06-24 17:22:29 +0300

    Import icon/button/toolbar in bookmark.module

--- Bookmark-Manager/src/app/bookmarks/bookmark/bookmark.module.ts
+++ Bookmark-Manager/src/app/bookmarks/bookmark/bookmark.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { RouterModule } from '@angular/router';
+
+// Angular Material
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { BookmarkComponent } from './bookmark.component';
-
-
 
 @NgModule({
   declarations: [
     BookmarkComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
   ]
 })
 export class BookmarkModule { }
