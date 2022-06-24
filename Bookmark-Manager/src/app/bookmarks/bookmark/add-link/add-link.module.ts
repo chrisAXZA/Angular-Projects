0ec618398062a0b792675c16dd60f3ad0d608d6f
chrisAXZA <nexter1@tutanota.com>
2022-06-24 17:30:11 +0300

    Add bookmark/add-link module/component

---
+++ Bookmark-Manager/src/app/bookmarks/bookmark/add-link/add-link.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AddLinkComponent } from './add-link.component';
+
+
+
+@NgModule({
+  declarations: [
+    AddLinkComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AddLinkModule { }
