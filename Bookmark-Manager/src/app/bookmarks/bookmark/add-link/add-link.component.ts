7dc8e6bc1c5fe095940bfea80d1426ab42974466
chrisAXZA <nexter1@tutanota.com>
2022-06-24 18:15:41 +0300

    Implement add-link styling

--- Bookmark-Manager/src/app/bookmarks/bookmark/add-link/add-link.component.ts
+++ Bookmark-Manager/src/app/bookmarks/bookmark/add-link/add-link.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-link',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddLinkComponent implements OnInit {
 
+  link = new FormControl('', [Validators.required]);
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  // will add a new link to the backend
+  addLink() {
+
+  }
+
+  getLinkError() {
+    if (this.link.hasError('required')) {
+      return 'You must enter a value!';
+    }
+
+    return '';
+  }
 }
