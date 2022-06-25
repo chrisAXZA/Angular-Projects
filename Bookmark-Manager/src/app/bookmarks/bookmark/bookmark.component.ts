71eec02f23a3c6ef0f02d3fa1596178c9eb1943b
chrisAXZA <nexter1@tutanota.com>
2022-06-25 14:16:05 +0300

    Add second switchMap inside ngOnInit

--- Bookmark-Manager/src/app/bookmarks/bookmark/bookmark.component.ts
+++ Bookmark-Manager/src/app/bookmarks/bookmark/bookmark.component.ts
@@ -6,8 +6,8 @@ import { switchMap } from 'rxjs';
 // Angular material
 import { MatDialog } from '@angular/material/dialog';
 
-import { Bookmark, BookmarkGQL } from 'src/generated-types';
 import { AddLinkComponent } from './add-link/add-link.component';
+import { Bookmark, BookmarkGQL, Link, LinksGQL } from 'src/generated-types';
 
 @Component({
   selector: 'app-bookmark',
@@ -17,11 +17,14 @@ import { AddLinkComponent } from './add-link/add-link.component';
 export class BookmarkComponent implements OnInit {
 
   bookmark: Bookmark;
+  links: Link[];
+  isLoading: boolean = true;
 
   constructor(
     private readonly route: ActivatedRoute, // activatedRoute injects the current route into the component
     private readonly bookmarkGql: BookmarkGQL,
-    private readonly dialog: MatDialog) { }
+    private readonly dialog: MatDialog,
+    private readonly linksGql: LinksGQL) { }
 
   // will listen to the changes in the route parameter
   ngOnInit(): void {
@@ -32,9 +35,20 @@ export class BookmarkComponent implements OnInit {
         switchMap((params) => {
           return this.bookmarkGql.watch({ _id: params['id'] }).valueChanges;
         }),
+        // add another switchMap in order provide another observable to this stream
+        // A second switchMap is required, since above bookmarkGql changes, this needs to be 
+        // reflected in the below links and cancel any ongoing calls
+        switchMap((result) => {
+          this.bookmark = result.data.bookmark;
+          return this.linksGql
+            .watch({ urls: result.data.bookmark.links })
+            .valueChanges;
+        }),
       )
       .subscribe((result) => {
-        this.bookmark = result.data.bookmark;
+        // this.bookmark = result.data.bookmark;
+        this.isLoading = result.loading;
+        this.links = result.data.links;
       });
   }
 
