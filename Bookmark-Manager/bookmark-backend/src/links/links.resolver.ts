caa7f8b6b98a435529958a31a40ca774e11eca90
chrisAXZA <nexter1@tutanota.com>
2022-06-25 14:00:24 +0300

    Add getLinks in links.services backend

--- Bookmark-Manager/bookmark-backend/src/links/links.resolver.ts
+++ Bookmark-Manager/bookmark-backend/src/links/links.resolver.ts
@@ -1,15 +1,19 @@
+import { UseGuards } from '@nestjs/common';
 import { Args, Query, Resolver } from '@nestjs/graphql';
 
 import { Link } from './link.model';
 import { LinksService } from './links.service';
+import { GetLinksArgs } from './dto/args/get-links-args.dto';
+import { GqlAuthGuard } from 'src/auth/guards/gql-auth.guard';
 
 @Resolver(() => Link)
 export class LinksResolver {
     constructor(private readonly linksService: LinksService) { }
 
-    @Query(() => [Link], {name: 'links'}) // will return an array of links, name is links
-    async getLinks(@Args() getLinksArgs){
-
+    @UseGuards(GqlAuthGuard) // protected route
+    @Query(() => [Link], { name: 'links' }) // will return an array of links, name is links
+    async getLinks(@Args() getLinksArgs: GetLinksArgs) {
+        return this.linksService.getLinks(getLinksArgs);
     }
 
 }
--- Bookmark-Manager/bookmark-backend/src/links/links.service.ts
+++ Bookmark-Manager/bookmark-backend/src/links/links.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { getLinkPreview } from 'link-preview-js';
+
+import { GetLinksArgs } from './dto/args/get-links-args.dto';
 
 @Injectable()
-export class LinksService {}
+export class LinksService {
+    // will iterate over links array and provide a link-preview for each one
+    async getLinks(getLinksArgs: GetLinksArgs) {
+        // will use Promise.All in order to execute all tasks (each url) in parallel (fetching links previews)
+        return Promise.all(
+            getLinksArgs.urls.map(async (url) => {
+                return getLinkPreview(url);
+            }),
+        );
+    }
+}
