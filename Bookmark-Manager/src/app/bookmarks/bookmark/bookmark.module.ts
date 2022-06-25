1268babb51dffbb6818ada659f1cedf953556a22
chrisAXZA <nexter1@tutanota.com>
2022-06-25 14:38:44 +0300

    Add material card/spinner in bookmark.module

--- Bookmark-Manager/src/app/bookmarks/bookmark/bookmark.module.ts
+++ Bookmark-Manager/src/app/bookmarks/bookmark/bookmark.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 // Angular Material
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { BookmarkComponent } from './bookmark.component';
 import { AddLinkModule } from './add-link/add-link.module';
@@ -17,10 +19,12 @@ import { AddLinkModule } from './add-link/add-link.module';
   imports: [
     CommonModule,
     RouterModule,
+    AddLinkModule,
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    AddLinkModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
   ]
 })
 export class BookmarkModule { }
