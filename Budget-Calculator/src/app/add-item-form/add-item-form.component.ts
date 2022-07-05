bf2f32a034cd08b53cb7e665eeabfdeedebaeecf
chrisAXZA <nexter1@tutanota.com>
2022-07-05 21:15:57 +0300

    Implement eventEmitter for BudgetItem in add-form

--- Budget-Calculator/src/app/add-item-form/add-item-form.component.ts
+++ Budget-Calculator/src/app/add-item-form/add-item-form.component.ts
@@ -1,26 +1,28 @@
 import { NgForm } from '@angular/forms';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 import { BudgetItem } from 'src/shared/modules/budget-item.model';
 
 @Component({
-  selector: 'app-add-item-form',
-  templateUrl: './add-item-form.component.html',
-  styleUrls: ['./add-item-form.component.scss']
+    selector: 'app-add-item-form',
+    templateUrl: './add-item-form.component.html',
+    styleUrls: ['./add-item-form.component.scss']
 })
 export class AddItemFormComponent implements OnInit {
 
-  // item needs to be set with default value
-  @Input() item: BudgetItem = new BudgetItem('', 0);
+    // item needs to be set with default value
+    @Input() item: BudgetItem = new BudgetItem('', 0);
+    @Output() formSubmit: EventEmitter<BudgetItem> = new EventEmitter<BudgetItem>();
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
-  onSubmit(form: NgForm) {
-    // console.log(`Value: ${form.value} >>> Valid: ${form.valid}`);
-    console.log(form.value, form.valid);
-  }
+    onSubmit(form: NgForm) {
+        // console.log(`Value: ${form.value} >>> Valid: ${form.valid}`);
+        // console.log(form.value, form.valid);
+        this.formSubmit.emit(form.value);
+    }
 
 }
