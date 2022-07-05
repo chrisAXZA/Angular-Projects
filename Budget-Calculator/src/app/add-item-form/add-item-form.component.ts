1dfc5590d84e77f835fd596619793e457e91483e
chrisAXZA <nexter1@tutanota.com>
2022-07-05 16:06:06 +0300

    Implement template validation+disable Add btn

--- Budget-Calculator/src/app/add-item-form/add-item-form.component.ts
+++ Budget-Calculator/src/app/add-item-form/add-item-form.component.ts
@@ -19,6 +19,7 @@ export class AddItemFormComponent implements OnInit {
   }
 
   onSubmit(form: NgForm) {
+    // console.log(`Value: ${form.value} >>> Valid: ${form.valid}`);
     console.log(form.value, form.valid);
   }
 
