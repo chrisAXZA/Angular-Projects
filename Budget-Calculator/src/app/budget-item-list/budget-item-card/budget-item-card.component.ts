e38360988fd000a00adff95a06e4aeac934e01d5
chrisAXZA <nexter1@tutanota.com>
2022-07-05 21:54:14 +0300

    Add dynamic rendering of income/expenses items

--- Budget-Calculator/src/app/budget-item-list/budget-item-card/budget-item-card.component.ts
+++ Budget-Calculator/src/app/budget-item-list/budget-item-card/budget-item-card.component.ts
@@ -1,17 +1,20 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+import { BudgetItem } from 'src/shared/modules/budget-item.model';
+
 @Component({
-  selector: 'app-budget-item-card',
-  templateUrl: './budget-item-card.component.html',
-  styleUrls: ['./budget-item-card.component.scss']
+    selector: 'app-budget-item-card',
+    templateUrl: './budget-item-card.component.html',
+    styleUrls: ['./budget-item-card.component.scss']
 })
 export class BudgetItemCardComponent implements OnInit {
 
-  @Input() isIncome: boolean = true;
+    // @Input() isIncome: boolean = true;
+    @Input() item!: BudgetItem;
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
