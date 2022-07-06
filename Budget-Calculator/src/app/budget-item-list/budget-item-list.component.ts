ee062433bb42cc2a5903eb0601e6c8b80892e968
chrisAXZA <nexter1@tutanota.com>
2022-07-06 21:00:01 +0300

    Import MatDialog and implement onCardClicked

--- Budget-Calculator/src/app/budget-item-list/budget-item-list.component.ts
+++ Budget-Calculator/src/app/budget-item-list/budget-item-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
 import { BudgetItem } from 'src/shared/modules/budget-item.model';
+import { EditItemModalComponent } from '../edit-item-modal/edit-item-modal.component';
 
 @Component({
     selector: 'app-budget-item-list',
@@ -13,7 +15,7 @@ export class BudgetItemListComponent implements OnInit {
     // parent component (mainPage) needs to know which BudgetItem needs to be deleted
     @Output() delete: EventEmitter<BudgetItem> = new EventEmitter<BudgetItem>();
 
-    constructor() { }
+    constructor(public dialog: MatDialog) { }
 
     ngOnInit(): void {
     }
@@ -24,5 +26,25 @@ export class BudgetItemListComponent implements OnInit {
         // this.budgetItems = this.budgetItems.filter((i) =>
         //     i.description !== item.description && i.amount !== item.amount);
         this.delete.emit(item);
+    }
+
+    onCardClicked(item: BudgetItem) {
+        // visualize the edit modal
+        const diaologRef = this.dialog.open(EditItemModalComponent, {
+            width: '580px',
+            data: item, // data receives the actual item that was clicked
+        });
+
+        // subscribe to the event when the dialog is closed
+        diaologRef.afterClosed()
+            .subscribe((result) => { // result is emitted by the closed dialog
+                // checks if result has a value
+                if (result){
+                    // result is the actual updated item
+                    // replace the actual item with the updated submitted item from the form
+                    const index  = this.budgetItems.indexOf(item);
+                    this.budgetItems[index] = result;
+                }
+            });
     }
 }
