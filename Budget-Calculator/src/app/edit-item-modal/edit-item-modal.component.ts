444674048d75ac5bec4f6207bd4476c2fb306b38
chrisAXZA <nexter1@tutanota.com>
2022-07-06 20:26:18 +0300

    Implement edit-item template

--- Budget-Calculator/src/app/edit-item-modal/edit-item-modal.component.ts
+++ Budget-Calculator/src/app/edit-item-modal/edit-item-modal.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { BudgetItem } from 'src/shared/modules/budget-item.model';
 
 @Component({
-  selector: 'app-edit-item-modal',
-  templateUrl: './edit-item-modal.component.html',
-  styleUrls: ['./edit-item-modal.component.scss']
+    selector: 'app-edit-item-modal',
+    templateUrl: './edit-item-modal.component.html',
+    styleUrls: ['./edit-item-modal.component.scss']
 })
 export class EditItemModalComponent implements OnInit {
 
-  constructor() { }
+    @Input() item!: BudgetItem;
 
-  ngOnInit(): void {
-  }
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+
+    onSubmitted(updateItem: BudgetItem) {
+
+    }
 
 }
