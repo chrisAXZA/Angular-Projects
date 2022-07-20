8a475e4bc445f78f1dff05d0a25d6001b1c32e5a
chrisAXZA <nexter1@tutanota.com>
2022-07-20 21:36:45 +0300

    Render tabs component in bottom-section

--- DashBoard/src/app/app.module.ts
+++ DashBoard/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TabsComponent } from './tabs/tabs.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
