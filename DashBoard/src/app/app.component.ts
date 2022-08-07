cdf406cb180179c67f50dfdafdf6e798cfde17ae
chrisAXZA <nexter1@tutanota.com>
2022-08-07 13:57:20 +0300

    Set transition opacity animation

--- DashBoard/src/app/app.component.ts
+++ DashBoard/src/app/app.component.ts
@@ -11,33 +11,50 @@ import { animate, query, style, transition, trigger } from '@angular/animations'
             transition('* => *', [
                 query(':enter', [
                     style({
-                        background: 'wheat',
+                        opacity: 0,
+                        // display needs to be set to block, as default inline will not be rendered correctly
                         display: 'block',
-                        // 100% height so that component does not take up more space 
-                        // than its parent element, however during transition Angular
-                        // will try to set height to auto, which is why specific styling
-                        // needs to be set in animate function
+                        // height needs to be set to 100% to avoid overflow
                         height: '100%',
                     }),
+                    // end-state has alreadt opacity styling, so no explicit style has to be set
+                    // in animate function
                     animate(700, style({
-                        // background: 'transparent',
-                        // * indicates that Angular needs to animate to the styles of the end-state
-                        // which in this case is transparent or rgba with 0 alpha
-                        // * takes up the styles of the end-state (and Angular won't try to transition
-                        // the height property anymore)
-                        background: '*', 
-                        // height: '*',
-                        // height: 'auto', // will transition heigh property
+                        opacity: 1,
                     })),
                 ], { optional: true }),
-                style({
-                    background: 'red',
-                }),
-                // animate(700, style({
-                //     background: 'violet',
-                // })),
-                animate(700),
             ]),
+            // * => * indicates transition from start-state to end-state
+            // transition('* => *', [
+            //     query(':enter', [
+            //         style({
+            //             background: 'wheat',
+            //             display: 'block',
+            //             // 100% height so that component does not take up more space 
+            //             // than its parent element, however during transition Angular
+            //             // will try to set height to auto, which is why specific styling
+            //             // needs to be set in animate function
+            //             height: '100%',
+            //         }),
+            //         animate(700, style({
+            //             // background: 'transparent',
+            //             // * indicates that Angular needs to animate to the styles of the end-state
+            //             // which in this case is transparent or rgba with 0 alpha
+            //             // * takes up the styles of the end-state (and Angular won't try to transition
+            //             // the height property anymore)
+            //             background: '*', 
+            //             // height: '*',
+            //             // height: 'auto', // will transition heigh property
+            //         })),
+            //     ], { optional: true }),
+            //     style({
+            //         background: 'red',
+            //     }),
+            //     // animate(700, style({
+            //     //     background: 'violet',
+            //     // })),
+            //     animate(700),
+            // ]),
         ]),
     ],
 })
