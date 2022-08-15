479e0cf23b692a924de01e17efea4a50046d4fea
chrisAXZA <nexter1@tutanota.com>
2022-08-15 10:21:16 +0300

    Add noteService in notes.component

--- DashBoard/src/app/notes/notes.component.ts
+++ DashBoard/src/app/notes/notes.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Note } from '../shared/note.model';
+import { NoteService } from '../shared/note.service';
+
 @Component({
   selector: 'app-notes',
   templateUrl: './notes.component.html',
@@ -7,9 +10,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotesComponent implements OnInit {
 
-  constructor() { }
+  notes!: Note[];
+
+  constructor(private noteService: NoteService) { }
 
   ngOnInit(): void {
+    this.notes = this.noteService.getNotes();
   }
 
 }
