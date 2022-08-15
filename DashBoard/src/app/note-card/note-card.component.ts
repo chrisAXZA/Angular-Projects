58d8529052b0f56f964f2781c749626e1ac36219
chrisAXZA <nexter1@tutanota.com>
2022-08-15 10:41:38 +0300

    Move note-card styling to note-card.component.scss

--- DashBoard/src/app/note-card/note-card.component.ts
+++ DashBoard/src/app/note-card/note-card.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { Note } from '../shared/note.model';
 
 @Component({
   selector: 'app-note-card',
@@ -6,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./note-card.component.scss']
 })
 export class NoteCardComponent implements OnInit {
+
+  @Input() note!: Note;
 
   constructor() { }
 
