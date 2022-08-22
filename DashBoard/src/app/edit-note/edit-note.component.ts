dd14011cfe6a5084f7a00e9bbe44277082b760e7
chrisAXZA <nexter1@tutanota.com>
2022-08-22 18:11:22 +0300

    Add routerLink to each note in note.component

--- DashBoard/src/app/edit-note/edit-note.component.ts
+++ DashBoard/src/app/edit-note/edit-note.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+
+import { NoteService } from '../shared/note.service';
 
 @Component({
   selector: 'app-edit-note',
@@ -7,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditNoteComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private noteService: NoteService) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      const idParam = paramMap.get('id')!;
+      const note = this.noteService.getNote(idParam);
+    });
+
   }
 
 }
