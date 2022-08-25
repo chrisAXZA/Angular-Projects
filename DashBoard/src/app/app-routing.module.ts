159bff43e355a2965393b1d8ee3c127effb093b8
chrisAXZA <nexter1@tutanota.com>
2022-08-25 16:15:04 +0300

    Add addTodo routing in app-routing

--- DashBoard/src/app/app-routing.module.ts
+++ DashBoard/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { NotesComponent } from './notes/notes.component';
 import { TodosComponent } from './todos/todos.component';
 import { AddNoteComponent } from './add-note/add-note.component';
+import { AddTodoComponent } from './add-todo/add-todo.component';
 import { EditNoteComponent } from './edit-note/edit-note.component';
 import { BookmarksComponent } from './bookmarks/bookmarks.component';
 
@@ -11,6 +12,7 @@ const routes: Routes = [
   // data will set the tabNumber of each Dashboard item
   { path: 'bookmarks', component: BookmarksComponent, data: { tab: 1 } },
   { path: 'todos', component: TodosComponent, data: { tab: 2 } },
+  { path: 'todos/add', component: AddTodoComponent,},
   { path: 'notes', component: NotesComponent, data: { tab: 3 } },
   { path: 'notes/add', component: AddNoteComponent, },
   { path: 'notes/:id', component: EditNoteComponent, },
