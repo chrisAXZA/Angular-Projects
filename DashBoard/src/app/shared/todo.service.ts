b238d5e620ba5de5e255340fc2108783af8cf46d
chrisAXZA <nexter1@tutanota.com>
2022-08-25 13:11:00 +0300

    Implement todo.service functionalities

--- DashBoard/src/app/shared/todo.service.ts
+++ DashBoard/src/app/shared/todo.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
 
+import { Todo } from './todo.model';
+
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class TodoService {
 
-  constructor() { }
+    todos!: Todo[];
+
+    constructor() { }
+
+    getTodos() {
+        return this.todos;
+    }
+
+    getTodo(id: string) {
+        const todo = this.todos.find((t) => t.id === id);
+        return todo;
+    }
+
+    addTodo(todo: Todo) {
+        this.todos.push(todo);
+    }
+
+    updateTodo(id: string, updatedTodoFields: Partial<Todo>) {
+        const todo = this.getTodo(id);
+        Object.assign(todo, updatedTodoFields);
+    }
+
+    deleteTodo(id: string) {
+        const todoIndex = this.todos.findIndex((t) => t.id === id);
+
+        if (todoIndex === -1) {
+            return;
+        }
+
+        this.todos.splice(todoIndex, 1);
+    }
 }
