41025a4781591c3073142e2a6cbc2e79e173296a
chrisAXZA <nexter1@tutanota.com>
2022-08-26 13:39:47 +0300

    Implement onEditClick in todos component

--- DashBoard/src/app/todos/todos.component.ts
+++ DashBoard/src/app/todos/todos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Todo } from '../shared/todo.model';
 import { TodoService } from '../shared/todo.service';
@@ -12,7 +13,7 @@ export class TodosComponent implements OnInit {
 
     todos!: Todo[];
 
-    constructor(private todoService: TodoService) { }
+    constructor(private todoService: TodoService, private router: Router) { }
 
     ngOnInit(): void {
         this.todos = this.todoService.getTodos();
@@ -20,5 +21,9 @@ export class TodosComponent implements OnInit {
 
     toggleCompleted(todo: Todo) {
         this.todoService.updateTodo(todo.id, { completed: !todo.completed });
+    }
+
+    onEditClick(todo: Todo){
+        this.router.navigate(['/todos', todo.id]);
     }
 }
