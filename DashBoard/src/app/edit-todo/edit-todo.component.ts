a50a2fa68146b0e3398c60ce6bc6b6dbc33f702e
chrisAXZA <nexter1@tutanota.com>
2022-09-02 16:47:30 +0300

    Remove showValidationErrors from edit-todo

--- DashBoard/src/app/edit-todo/edit-todo.component.ts
+++ DashBoard/src/app/edit-todo/edit-todo.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { ActivatedRoute, ParamMap } from '@angular/router';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 
 import { Todo } from '../shared/todo.model';
 
@@ -15,7 +15,11 @@ export class EditTodoComponent implements OnInit {
 
     todo!: Todo;
 
-    constructor(private route: ActivatedRoute, private todoService: TodoService) { }
+    constructor(
+        private route: ActivatedRoute,
+        private todoService: TodoService,
+        private router: Router
+    ) { }
 
     ngOnInit(): void {
         this.route.paramMap.subscribe((paramMap: ParamMap) => {
@@ -25,7 +29,12 @@ export class EditTodoComponent implements OnInit {
         });
     }
 
-    onFormSubmit(form: NgForm){
+    onFormSubmit(form: NgForm) {
+        if(form.invalid){
+            return;
+        }
+        
         this.todoService.updateTodo(this.todo.id, form.value);
+        this.router.navigateByUrl('/todos');
     }
 }
