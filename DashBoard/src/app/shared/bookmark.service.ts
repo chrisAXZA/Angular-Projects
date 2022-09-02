833ebbd4fd60239eeaa6c8a4bd40e06bbc72d96d
chrisAXZA <nexter1@tutanota.com>
2022-09-02 19:53:15 +0300

    Implement bookmark.service functionalities

--- DashBoard/src/app/shared/bookmark.service.ts
+++ DashBoard/src/app/shared/bookmark.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
 
+import { Bookmark } from './bookmark.model';
+
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class BookmarkService {
 
-  constructor() { }
+    bookmarks!: Bookmark[];
+
+    constructor() { }
+
+    getBookmarks() {
+        return this.bookmarks;
+    }
+
+    getBookmark(id: string) {
+        return this.bookmarks.find((book) => book.id === id);
+    }
+
+    addBookmark(bookmark: Bookmark) {
+        this.bookmarks.push(bookmark);
+    }
+
+    updateBookmark(id: string, updatedFields: Partial<Bookmark>) {
+        const currentBookmark = this.getBookmark(id);
+
+        if (currentBookmark) {
+            Object.assign(currentBookmark, updatedFields);
+        }
+    }
+
+    deleteBookmark(id: string) {
+        const index = this.bookmarks.findIndex((book) => book.id === id);
+
+        if (index !== -1) {
+            this.bookmarks.splice(index, 1);
+        }
+    }
 }
