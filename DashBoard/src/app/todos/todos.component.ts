de6ad81d91108871994f1dd07d53f9cd3f12f9e3
chrisAXZA <nexter1@tutanota.com>
2022-09-02 14:57:15 +0300

    Add fade-out animation for onDeleteClick todo

--- DashBoard/src/app/todos/todos.component.ts
+++ DashBoard/src/app/todos/todos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 import { Todo } from '../shared/todo.model';
 import { TodoService } from '../shared/todo.service';
@@ -7,7 +8,18 @@ import { TodoService } from '../shared/todo.service';
 @Component({
     selector: 'app-todos',
     templateUrl: './todos.component.html',
-    styleUrls: ['./todos.component.scss']
+    styleUrls: ['./todos.component.scss'],
+    animations: [
+        trigger('todoItemAnim', [
+            transition(':leave', [
+                animate(500, style({
+                    opacity: 0,
+                    height: 0,
+                    marginBottom: 0,
+                })),
+            ]),
+        ]),
+    ],
 })
 export class TodosComponent implements OnInit {
 
