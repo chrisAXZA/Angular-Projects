16a1198fe806c0e85adb06d5b62d5d9cc3b80148
chrisAXZA <nexter1@tutanota.com>
2022-09-03 21:37:23 +0300

    Set edit-bookmark route as child manage-bookmark

--- DashBoard/src/app/app-routing.module.ts
+++ DashBoard/src/app/app-routing.module.ts
@@ -16,8 +16,10 @@ const routes: Routes = [
   // data will set the tabNumber of each Dashboard item
   { path: 'bookmarks', component: BookmarksComponent, data: { tab: 1 } },
   { path: 'bookmarks/add', component: AddBookmarkComponent, },
-  { path: 'bookmarks/manage', component: ManageBookmarksComponent, },
-  { path: 'bookmarks/manage/:id', component: EditBookmarkComponent, },
+  { path: 'bookmarks/manage', component: ManageBookmarksComponent, children: [
+      { path: ':id', component: EditBookmarkComponent, },
+    ]
+  },
   { path: 'todos', component: TodosComponent, data: { tab: 2 } },
   { path: 'todos/add', component: AddTodoComponent, },
   { path: 'todos/:id', component: EditTodoComponent, },
