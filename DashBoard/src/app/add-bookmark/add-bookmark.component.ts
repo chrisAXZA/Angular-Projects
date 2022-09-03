567748efcd49073f5c2716d3cd34bf3ec8b56c13
chrisAXZA <nexter1@tutanota.com>
2022-09-03 11:29:03 +0300

    Implement addBookmark routing in app-routing

--- DashBoard/src/app/add-bookmark/add-bookmark.component.ts
+++ DashBoard/src/app/add-bookmark/add-bookmark.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-add-bookmark',
-  templateUrl: './add-bookmark.component.html',
-  styleUrls: ['./add-bookmark.component.scss']
+    selector: 'app-add-bookmark',
+    templateUrl: './add-bookmark.component.html',
+    styleUrls: ['./add-bookmark.component.scss']
 })
 export class AddBookmarkComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
