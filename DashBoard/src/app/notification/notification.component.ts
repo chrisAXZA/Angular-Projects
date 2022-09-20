5fe716e0947dbc1d423e36db97890f7683377cc5
chrisAXZA <nexter1@tutanota.com>
2022-09-20 13:23:44 +0300

    Add notificationService in notification.component

--- DashBoard/src/app/notification/notification.component.ts
+++ DashBoard/src/app/notification/notification.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { NotificationService } from '../shared/notification.service';
+
 @Component({
-  selector: 'app-notification',
-  templateUrl: './notification.component.html',
-  styleUrls: ['./notification.component.scss']
+    selector: 'app-notification',
+    templateUrl: './notification.component.html',
+    styleUrls: ['./notification.component.scss']
 })
 export class NotificationComponent implements OnInit {
 
-  constructor() { }
+    notification!: string;
 
-  ngOnInit(): void {
-  }
+    constructor(private notificationService: NotificationService) { }
+
+    ngOnInit(): void {
+        this.notificationService.notifications.subscribe((notification: string) => {
+            this.notification = notification;
+        });
+    }
 
 }
