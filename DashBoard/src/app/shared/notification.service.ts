b69b8aa4815405a7ccbf3e2afe4db7f51d0a721f
chrisAXZA <nexter1@tutanota.com>
2022-09-20 13:20:57 +0300

    Add show functionality+get in notification service

--- DashBoard/src/app/shared/notification.service.ts
+++ DashBoard/src/app/shared/notification.service.ts
@@ -7,11 +7,20 @@ import { Subject } from 'rxjs';
 export class NotificationService {
 
     // Subject
-    notification$: Subject<String> = new Subject();
+    private notification$: Subject<string> = new Subject();
+
+    get notifications() {
+        // since notifications$ is private outside code can access it
+        // as an Observable but can not use its Subject-functionality
+        // (ie call next()) and only subscribe to its values
+        return this.notification$.asObservable();
+    }
 
     constructor() { }
 
-    show() {
-
+    show(text: string) {
+        // whenever show method is being executed, the notification text will
+        // be sent to all of its subscribers
+        this.notification$.next(text);
     }
 }
