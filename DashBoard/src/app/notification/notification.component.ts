4b2032398f561c5d8411b6fe851e1efd8f423af1
chrisAXZA <nexter1@tutanota.com>
2022-09-21 21:21:06 +0300

    Implement clearTimeout in notification component

--- DashBoard/src/app/notification/notification.component.ts
+++ DashBoard/src/app/notification/notification.component.ts
@@ -11,13 +11,17 @@ export class NotificationComponent implements OnInit {
 
     notification!: string | null;
 
+    timeout: any;
+
     constructor(private notificationService: NotificationService) { }
 
     ngOnInit(): void {
         this.notificationService.notifications.subscribe((notification: string) => {
             this.notification = notification;
 
-            setTimeout(() => {
+            clearTimeout(this.timeout);
+
+            this.timeout = setTimeout(() => {
                 this.notification = null;
             }, 2500);
         });
