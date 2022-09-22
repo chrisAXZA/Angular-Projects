4987205f0ef57ae8fdd82f4f2186d70aa72f71d9
chrisAXZA <nexter1@tutanota.com>
2022-09-22 16:26:18 +0300

    Add notification for editing note in edit-note

--- DashBoard/src/app/edit-note/edit-note.component.ts
+++ DashBoard/src/app/edit-note/edit-note.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 
 import { Note } from '../shared/note.model';
 import { NoteService } from '../shared/note.service';
+import { NotificationService } from '../shared/notification.service';
 
 @Component({
     selector: 'app-edit-note',
@@ -17,7 +18,9 @@ export class EditNoteComponent implements OnInit {
     constructor(
         private route: ActivatedRoute,
         private noteService: NoteService,
-        private router: Router) { }
+        private notificationService: NotificationService,
+        private router: Router,
+    ) { }
 
     ngOnInit(): void {
         this.route.paramMap.subscribe((paramMap: ParamMap) => {
@@ -34,6 +37,7 @@ export class EditNoteComponent implements OnInit {
     onFormSubmit(form: NgForm) {
         // console.log(form.value);
         this.noteService.updateNote(this.note.id, form.value);
+        this.notificationService.show(`${this.note.title} has been updated!`);
         this.router.navigateByUrl("/notes");
     }
 
