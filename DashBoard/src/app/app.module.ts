40d1164cba741200480f97b332f5d7444dbaa497
chrisAXZA <nexter1@tutanota.com>
2022-09-29 13:44:26 +0300

    Implement change-background button in app template

--- DashBoard/src/app/app.module.ts
+++ DashBoard/src/app/app.module.ts
@@ -9,17 +9,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { NotesComponent } from './notes/notes.component';
 import { TodosComponent } from './todos/todos.component';
 import { AddNoteComponent } from './add-note/add-note.component';
+import { AddTodoComponent } from './add-todo/add-todo.component';
 import { NoteCardComponent } from './note-card/note-card.component';
-import { BookmarksComponent } from './bookmarks/bookmarks.component';
-import { BookmarkTileComponent } from './bookmark-tile/bookmark-tile.component';
 import { EditNoteComponent } from './edit-note/edit-note.component';
 import { TodoItemComponent } from './todo-item/todo-item.component';
-import { AddTodoComponent } from './add-todo/add-todo.component';
 import { EditTodoComponent } from './edit-todo/edit-todo.component';
+import { BookmarksComponent } from './bookmarks/bookmarks.component';
 import { AddBookmarkComponent } from './add-bookmark/add-bookmark.component';
-import { ManageBookmarksComponent } from './manage-bookmarks/manage-bookmarks.component';
-import { EditBookmarkComponent } from './edit-bookmark/edit-bookmark.component';
 import { NotificationComponent } from './notification/notification.component';
+import { BookmarkTileComponent } from './bookmark-tile/bookmark-tile.component';
+import { EditBookmarkComponent } from './edit-bookmark/edit-bookmark.component';
+import { ManageBookmarksComponent } from './manage-bookmarks/manage-bookmarks.component';
 
 @NgModule({
   declarations: [
