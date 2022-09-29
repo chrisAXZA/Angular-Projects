d05c254848dda58c0880a7c35f20bdc7d08d1e43
chrisAXZA <nexter1@tutanota.com>
2022-09-29 14:24:49 +0300

    Implement background-img styling in app.component

--- DashBoard/src/app/app.component.ts
+++ DashBoard/src/app/app.component.ts
@@ -152,7 +152,8 @@ import { animate, group, query, style, transition, trigger } from '@angular/anim
 })
 export class AppComponent {
     title = 'DashBoard';
-    background: string = 'https://images.unsplash.com/photo-1664425216679-dff9fb34913b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzMHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60';
+    background: string = 'https://images.unsplash.com/photo-1664369058082-ee8e36028106?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80';
+    // background: string = 'https://images.unsplash.com/photo-1664425216679-dff9fb34913b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzMHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60';
 
     prepareRoute(outlet: RouterOutlet) {
         if (outlet.isActivated) {
