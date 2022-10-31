5a1743d4bc3a5b7b786a0e867f5f08c620b93962
chrisAXZA <nexter1@tutanota.com>
2022-10-31 15:28:35 +0200

    Implement pokemon.ts

--- Pesho-Mon/src/app/app.component.ts
+++ Pesho-Mon/src/app/app.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-root',
-    template: `<h1> Welcome to {{title}}! </h1>`,
+    template: `<h1> Welcome to {{pokemonList[0]}}! </h1>`,
 })
-export class AppComponent {
-    title = 'Pesho-Mon';
+export class AppComponent implements OnInit {
+    pokemonList = ['Bulbar', 'Salamander', 'Sheller'];
+
+    ngOnInit() {
+        console.table(this.pokemonList);
+        this.selectPokemon(this.pokemonList[0]);
+    }
+
+    selectPokemon(pokemonName: string) {
+        console.log(`Pokemon with the name "${pokemonName}" has been clicked!`);
+
+    }
 }
