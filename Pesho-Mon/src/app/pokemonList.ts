0a86fda83eb133a17e82821a8a01dd8c95889a2b
chrisAXZA <nexter1@tutanota.com>
2022-10-31 15:37:05 +0200

    Implement pokemonList.ts

---
+++ Pesho-Mon/src/app/pokemonList.ts
@@ -0,0 +1,112 @@
+import { Pokemon } from './pokemon';
+
+export const POKEMONS: Pokemon[] = [
+    {
+        id: 1,
+        name: 'Bulbar',
+        hp: 25,
+        cp: 5,
+        picture: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png',
+        types: ['Plant', 'Poison'],
+        created: new Date()
+    },
+    {
+        id: 2,
+        name: 'Salamander',
+        hp: 28,
+        cp: 6,
+        picture: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/004.png',
+        types: ['Fire'],
+        created: new Date()
+    },
+    {
+        id: 3,
+        name: 'Sheller',
+        hp: 21,
+        cp: 4,
+        picture: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/007.png',
+        types: ['Water'],
+        created: new Date()
+    },
+    {
+        id: 4,
+        name: 'Aprikoto',
+        hp: 16,
+        cp: 2,
+        picture: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/013.png',
+        types: ['Insect', 'Poison'],
+        created: new Date()
+    },
+    {
+        id: 5,
+        name: 'Rucola',
+        hp: 30,
+        cp: 7,
+        picture: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/016.png',
+        types: ['Normal', 'Flying'],
+        created: new Date()
+    },
+    {
+        id: 6,
+        name: 'Rattata',
+        hp: 18,
+        cp: 6,
+        picture: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/019.png',
+        types: ['Normal'],
+        created: new Date()
+    },
+    {
+        id: 7,
+        name: 'Petebec',
+        hp: 14,
+        cp: 5,
+        picture: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/021.png',
+        types: ['Normal', 'Flying'],
+        created: new Date()
+    },
+    {
+        id: 8,
+        name: 'Aboo',
+        hp: 16,
+        cp: 4,
+        picture: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/023.png',
+        types: ['Poison'],
+        created: new Date()
+    },
+    {
+        id: 9,
+        name: 'Pikachu',
+        hp: 21,
+        cp: 7,
+        picture: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/025.png',
+        types: ['Electric'],
+        created: new Date()
+    },
+    {
+        id: 10,
+        name: 'Sabertiger',
+        hp: 19,
+        cp: 3,
+        picture: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/027.png',
+        types: ['Normal'],
+        created: new Date()
+    },
+    {
+        id: 11,
+        name: 'Meedixi',
+        hp: 25,
+        cp: 5,
+        picture: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/035.png',
+        types: ['Fairy'],
+        created: new Date()
+    },
+    {
+        id: 12,
+        name: 'Grouper',
+        hp: 17,
+        cp: 8,
+        picture: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/037.png',
+        types: ['Fire'],
+        created: new Date()
+    },
+];
