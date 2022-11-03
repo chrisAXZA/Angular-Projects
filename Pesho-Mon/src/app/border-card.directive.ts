aec9b3dce224601b120cfc5a934c19c9578df75e
chrisAXZA <nexter1@tutanota.com>
2022-11-03 21:14:03 +0200

    Implement onMouseEnter+onMouseLeave in border-card

--- Pesho-Mon/src/app/border-card.directive.ts
+++ Pesho-Mon/src/app/border-card.directive.ts
@@ -1,20 +1,28 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
     selector: '[pokemonBorderCard]'
 })
 export class BorderCardDirective {
 
-    constructor(private elementRef: ElementRef) { 
+    constructor(private elementRef: ElementRef) {
         this.setHeight(180);
         this.setBorder('#f5f5f5');
     }
 
-    setHeight(height: number) {
+    @HostListener('mouseenter') onMouseEnter() {
+        this.setBorder('#009688');
+    }
+
+    @HostListener('mouseleave') onMouseLeave() {
+        this.setBorder('#f5f5f5');
+    }
+
+    private setHeight(height: number) {
         this.elementRef.nativeElement.style.height = `${height}px`;
     }
 
-    setBorder(color: string) {
+    private setBorder(color: string) {
         this.elementRef.nativeElement.style.border = `solid 4px ${color}`;
     }
 }
