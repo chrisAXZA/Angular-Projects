3d6a8ce8d9d23fa044a4c7de5d1a48d35d55a89f
chrisAXZA <nexter1@tutanota.com>
2022-11-10 11:04:49 +0200

    Add app.component.scss+pokemonCard style

--- Pesho-Mon/src/app/app.component.ts
+++ Pesho-Mon/src/app/app.component.ts
@@ -6,6 +6,7 @@ import { POKEMONS } from './pokemonList';
 @Component({
     selector: 'app-root',
     templateUrl: 'app.component.html',
+    styleUrls : ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
     pokemonList: Pokemon[] = POKEMONS;
--- Pesho-Mon/src/app/pokemon-type-color.pipe.ts
+++ Pesho-Mon/src/app/pokemon-type-color.pipe.ts
@@ -50,5 +50,4 @@ export class PokemonTypeColorPipe implements PipeTransform {
         // chip materialize class icon
         return 'chip ' + color;
     }
-
 }
