3aecf51a926ad95b606cfa522a35f63d5f7e84aa
chrisAXZA <nexter1@tutanota.com>
2022-11-12 15:31:32 +0200

    Refactor app-routing+remove backslash

--- Pesho-Mon/src/app/app-routing.module.ts
+++ Pesho-Mon/src/app/app-routing.module.ts
@@ -5,9 +5,9 @@ import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.component';
 
 const routes: Routes = [
-    { path: '/pokemons', component: PokemonListComponent },
-    { path: '/pokemon/:id', component: PokemonDetailComponent },
-    { path: '', redirectTo: '/pokemons', pathMatch: 'full', },
+    { path: 'pokemons', component: PokemonListComponent },
+    { path: 'pokemon/:id', component: PokemonDetailComponent },
+    { path: '', redirectTo: 'pokemons', pathMatch: 'full', },
 ];
 
 @NgModule({
