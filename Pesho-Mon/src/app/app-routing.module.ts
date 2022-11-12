4137dab3559d4e4c14101fbd7bdd32aad1a09ce6
chrisAXZA <nexter1@tutanota.com>
2022-11-12 15:29:25 +0200

    Add pokemon-list/detail routes in app-routing

--- Pesho-Mon/src/app/app-routing.module.ts
+++ Pesho-Mon/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.component';
+
+const routes: Routes = [
+    { path: '/pokemons', component: PokemonListComponent },
+    { path: '/pokemon/:id', component: PokemonDetailComponent },
+    { path: '', redirectTo: '/pokemons', pathMatch: 'full', },
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
