98fa9ad3ef3b0167a2a7130244c7d2f05c6b682b
chrisAXZA <nexter1@tutanota.com>
2022-11-13 17:13:44 +0200

    Implement pokemon-list template+component

--- Pesho-Mon/src/app/app.component.ts
+++ Pesho-Mon/src/app/app.component.ts
@@ -1,34 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Pokemon } from './pokemon';
-import { POKEMONS } from './pokemonList';
-
 @Component({
     selector: 'app-root',
     templateUrl: 'app.component.html',
-    styleUrls : ['./app.component.scss'],
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-    pokemonList: Pokemon[] = POKEMONS;
-    pokemonSelected: Pokemon | undefined;
 
-    ngOnInit() {
-        console.table(this.pokemonList);
-    }
-
-    selectPokemon(pokemonId: string) {
-        // const index: number = Number((event.target as HTMLInputElement).value);
-        const index = Number(pokemonId);
-        // const pokemon : Pokemon = this.pokemonList[index];
-        const pokemon: Pokemon | undefined = this.pokemonList
-            .find((pok) => pok.id === index);
-
-        if (pokemon) {
-            console.log(`Pokemon with the name "${pokemon.name}" has been clicked!`);
-            this.pokemonSelected = pokemon;
-        } else {
-            console.log(`Pokemon with the id "${index}" does not exist!`);
-            this.pokemonSelected = undefined;
-        }
-    }
+    ngOnInit() { }
+    
 }
--- Pesho-Mon/src/app/pokemon-list/pokemon-list.component.ts
+++ Pesho-Mon/src/app/pokemon-list/pokemon-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Pokemon } from '../pokemon';
+import { POKEMONS } from '../pokemonList';
+
 @Component({
     selector: 'app-pokemon-list',
     templateUrl: './pokemon-list.component.html',
@@ -7,10 +10,28 @@ import { Component, OnInit } from '@angular/core';
     ]
 })
 export class PokemonListComponent implements OnInit {
+    pokemonList: Pokemon[] = POKEMONS;
+    pokemonSelected: Pokemon | undefined;
 
     constructor() { }
 
     ngOnInit(): void {
+    }
+
+    selectPokemon(pokemonId: string) {
+        // const index: number = Number((event.target as HTMLInputElement).value);
+        const index = Number(pokemonId);
+        // const pokemon : Pokemon = this.pokemonList[index];
+        const pokemon: Pokemon | undefined = this.pokemonList
+            .find((pok) => pok.id === index);
+
+        if (pokemon) {
+            console.log(`Pokemon with the name "${pokemon.name}" has been clicked!`);
+            this.pokemonSelected = pokemon;
+        } else {
+            console.log(`Pokemon with the id "${index}" does not exist!`);
+            this.pokemonSelected = undefined;
+        }
     }
 
 }
