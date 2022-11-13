9816a9a757ff786b6b3ec1791cae6d0739509e38
chrisAXZA <nexter1@tutanota.com>
2022-11-13 18:15:51 +0200

    Implement pokemon-detail template

--- Pesho-Mon/src/app/pokemon-detail/pokemon-detail.component.ts
+++ Pesho-Mon/src/app/pokemon-detail/pokemon-detail.component.ts
@@ -1,16 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Pokemon } from '../pokemon';
+import { POKEMONS } from '../pokemonList';
 
 @Component({
-  selector: 'app-pokemon-detail',
-  templateUrl: './pokemon-detail.component.html',
-  styles: [
-  ]
+    selector: 'app-pokemon-detail',
+    templateUrl: './pokemon-detail.component.html',
 })
 export class PokemonDetailComponent implements OnInit {
+    pokemonList: Pokemon[] = POKEMONS;
+    pokemon: Pokemon;
 
-  constructor() { }
+    constructor(private route: ActivatedRoute) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        // snapshot takes current url data, paramMap represents a table containing all of its
+        // parameters contained in paramMap
+        const pokemonId: number = Number(this.route.snapshot.paramMap.get('id'));
+        const currentPokemon: Pokemon | undefined = this.pokemonList.find((p) => p.id === pokemonId);
+
+        if (!currentPokemon) {
+            alert(`Peshomon with id "${pokemonId}" does not exist!`);
+        }
+
+        this.pokemon = currentPokemon!;
+    }
+
+    return() {
+
+    }
 
 }
