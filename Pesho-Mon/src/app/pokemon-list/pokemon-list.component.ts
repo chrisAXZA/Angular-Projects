c8458c7074cec4efea05d3218dafbe3c94101cc9
chrisAXZA <nexter1@tutanota.com>
2022-11-13 18:21:49 +0200

    Implement pokemon-list styles

--- Pesho-Mon/src/app/pokemon-list/pokemon-list.component.ts
+++ Pesho-Mon/src/app/pokemon-list/pokemon-list.component.ts
@@ -6,6 +6,7 @@ import { POKEMONS } from '../pokemonList';
 @Component({
     selector: 'app-pokemon-list',
     templateUrl: './pokemon-list.component.html',
+    styleUrls:['./pokemon-list.component.scss'],
 })
 export class PokemonListComponent implements OnInit {
     pokemonList: Pokemon[] = POKEMONS;
