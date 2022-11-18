bc32cdf94bdf07d1247b1e30f8f99ec0cbfaa240
chrisAXZA <nexter1@tutanota.com>
2022-11-18 16:50:30 +0200

    Implement page-not-found template

--- Pesho-Mon/src/app/page-not-found/page-not-found.component.ts
+++ Pesho-Mon/src/app/page-not-found/page-not-found.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-page-not-found',
@@ -6,11 +6,4 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class PageNotFoundComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class PageNotFoundComponent { }
