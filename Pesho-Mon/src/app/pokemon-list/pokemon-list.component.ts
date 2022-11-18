2084dd74da9ea92ef93e235bdbf99c30c406f04a
chrisAXZA <nexter1@tutanota.com>
2022-11-18 16:13:20 +0200

    Implement selectPokemonDetail in pokemon-list

--- Pesho-Mon/src/app/pokemon-list/pokemon-list.component.ts
+++ Pesho-Mon/src/app/pokemon-list/pokemon-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Pokemon } from '../pokemon';
 import { POKEMONS } from '../pokemonList';
@@ -6,28 +7,37 @@ import { POKEMONS } from '../pokemonList';
 @Component({
     selector: 'app-pokemon-list',
     templateUrl: './pokemon-list.component.html',
-    styleUrls:['./pokemon-list.component.scss'],
+    styleUrls: ['./pokemon-list.component.scss'],
 })
 export class PokemonListComponent implements OnInit {
     pokemonList: Pokemon[] = POKEMONS;
     pokemonSelected: Pokemon | undefined;
 
+    constructor(private router: Router) {
+
+    }
+
     ngOnInit(): void { }
 
-    selectPokemon(pokemonId: string) {
-        // const index: number = Number((event.target as HTMLInputElement).value);
-        const index = Number(pokemonId);
-        // const pokemon : Pokemon = this.pokemonList[index];
-        const pokemon: Pokemon | undefined = this.pokemonList
-            .find((pok) => pok.id === index);
 
-        if (pokemon) {
-            console.log(`Pokemon with the name "${pokemon.name}" has been clicked!`);
-            this.pokemonSelected = pokemon;
-        } else {
-            console.log(`Pokemon with the id "${index}" does not exist!`);
-            this.pokemonSelected = undefined;
-        }
+    // selectPokemon(pokemonId: string) {
+    //     // const index: number = Number((event.target as HTMLInputElement).value);
+    //     const index = Number(pokemonId);
+    //     // const pokemon : Pokemon = this.pokemonList[index];
+    //     const pokemon: Pokemon | undefined = this.pokemonList
+    //         .find((pok) => pok.id === index);
+
+    //     if (pokemon) {
+    //         console.log(`Pokemon with the name "${pokemon.name}" has been clicked!`);
+    //         this.pokemonSelected = pokemon;
+    //     } else {
+    //         console.log(`Pokemon with the id "${index}" does not exist!`);
+    //         this.pokemonSelected = undefined;
+    //     }
+    // }
+
+    selectPokemonDetail(pokemon: Pokemon) {
+        this.router.navigate(['/pokemon', pokemon.id]);
     }
 
 }
