9ee22922a470e2e2cb2a23ba2690c1d1d507d126
chrisAXZA <nexter1@tutanota.com>
2022-11-19 19:58:05 +0200

    Clean up code in app-routing+app modules

--- Pesho-Mon/src/app/app-routing.module.ts
+++ Pesho-Mon/src/app/app-routing.module.ts
@@ -2,12 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-// import { PokemonListComponent } from './peshomon/pokemon-list/pokemon-list.component';
-// import { PokemonDetailComponent } from './peshomon/pokemon-detail/pokemon-detail.component';
 
 const routes: Routes = [
-    // { path: 'pokemons', component: PokemonListComponent },
-    // { path: 'pokemon/:id', component: PokemonDetailComponent },
     { path: '', redirectTo: 'pokemons', pathMatch: 'full', },
     { path: '**', component: PageNotFoundComponent },
 ];
--- Pesho-Mon/src/app/app.module.ts
+++ Pesho-Mon/src/app/app.module.ts
@@ -5,18 +5,10 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { PeshomonModule } from './peshomon/peshomon.module';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-// import { BorderCardDirective } from './peshomon/border-card.directive';
-// import { PokemonTypeColorPipe } from './peshomon/pokemon-type-color.pipe';
-// import { PokemonListComponent } from './peshomon/pokemon-list/pokemon-list.component';
-// import { PokemonDetailComponent } from './peshomon/pokemon-detail/pokemon-detail.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        // BorderCardDirective,
-        // PokemonTypeColorPipe,
-        // PokemonListComponent,
-        // PokemonDetailComponent,
         PageNotFoundComponent,
     ],
     imports: [
