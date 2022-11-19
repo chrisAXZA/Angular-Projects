71a1895099ca193606d89e38e321d1b24f9ae0e2
chrisAXZA <nexter1@tutanota.com>
2022-11-19 19:45:02 +0200

    Refactor declarations in app+peshomon modules

--- Pesho-Mon/src/app/app.module.ts
+++ Pesho-Mon/src/app/app.module.ts
@@ -3,24 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { BorderCardDirective } from './peshomon/border-card.directive';
-import { PokemonTypeColorPipe } from './peshomon/pokemon-type-color.pipe';
+import { PeshomonModule } from './peshomon/peshomon.module';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { PokemonListComponent } from './peshomon/pokemon-list/pokemon-list.component';
-import { PokemonDetailComponent } from './peshomon/pokemon-detail/pokemon-detail.component';
+// import { BorderCardDirective } from './peshomon/border-card.directive';
+// import { PokemonTypeColorPipe } from './peshomon/pokemon-type-color.pipe';
+// import { PokemonListComponent } from './peshomon/pokemon-list/pokemon-list.component';
+// import { PokemonDetailComponent } from './peshomon/pokemon-detail/pokemon-detail.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        BorderCardDirective,
-        PokemonTypeColorPipe,
-        PokemonListComponent,
-        PokemonDetailComponent,
+        // BorderCardDirective,
+        // PokemonTypeColorPipe,
+        // PokemonListComponent,
+        // PokemonDetailComponent,
         PageNotFoundComponent,
     ],
     imports: [
         BrowserModule,
         AppRoutingModule,
+        PeshomonModule,
     ],
     providers: [],
     bootstrap: [AppComponent]
--- Pesho-Mon/src/app/peshomon/peshomon.module.ts
+++ Pesho-Mon/src/app/peshomon/peshomon.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { BorderCardDirective } from './border-card.directive';
+import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.component';
+
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-  ],
+    declarations: [
+        PokemonListComponent,
+        PokemonDetailComponent,
+        BorderCardDirective,
+        PokemonTypeColorPipe,
+    ],
+    imports: [
+        CommonModule,
+    ],
 })
 export class PeshomonModule { }
