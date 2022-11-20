abceb081eac79bd4aa80a25b842182ae62ee0f82
chrisAXZA <nexter1@tutanota.com>
2022-11-20 20:53:35 +0200

    Add peshomon-form component

---
+++ Pesho-Mon/src/app/peshomon/peshomon-form/peshomon-form.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-peshomon-form',
+  templateUrl: './peshomon-form.component.html',
+  styles: [
+  ]
+})
+export class PeshomonFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- Pesho-Mon/src/app/peshomon/peshomon.module.ts
+++ Pesho-Mon/src/app/peshomon/peshomon.module.ts
@@ -8,6 +8,7 @@ import { BorderCardDirective } from './border-card.directive';
 import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.component';
+import { PeshomonFormComponent } from './peshomon-form/peshomon-form.component';
 
 const peshomonRoutes: Routes = [
     { path: 'pokemons', component: PokemonListComponent, },
@@ -20,6 +21,7 @@ const peshomonRoutes: Routes = [
         PokemonDetailComponent,
         BorderCardDirective,
         PokemonTypeColorPipe,
+        PeshomonFormComponent,
     ],
     imports: [
         CommonModule,
