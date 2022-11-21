48efb3057a024a061f4b42a28feead2d518af2db
chrisAXZA <nexter1@tutanota.com>
2022-11-21 11:31:55 +0200

    Implement onSubmit+selectType functionality

--- Pesho-Mon/src/app/peshomon/peshomon-form/peshomon-form.component.ts
+++ Pesho-Mon/src/app/peshomon/peshomon-form/peshomon-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import Pokemon from '../pokemon';
 import PeshomonService from '../peshomon.service';
@@ -13,14 +14,16 @@ export class PeshomonFormComponent implements OnInit {
     @Input() peshomon: Pokemon;
     peshomonTypes: string[];
 
-    constructor(private peshomonService: PeshomonService) { }
+    constructor(private peshomonService: PeshomonService, private router: Router) { }
 
     ngOnInit(): void {
         this.peshomonTypes = this.peshomonService.getPeshomonTypeList();
     }
 
     onSubmit() {
+        console.log(`Peshomon data of ${this.peshomon.name} has been updated!`);
 
+        this.router.navigate(['/pokemon', this.peshomon.id]);
     }
 
     hasType(type: string): boolean {
@@ -31,7 +34,13 @@ export class PeshomonFormComponent implements OnInit {
         return this.peshomonTypes.includes(type);
     }
 
-    selectType() {
+    selectType($event: Event, type: string) {
+        const isChecked: boolean = ($event.target as HTMLInputElement).checked;
 
+        if (isChecked) {
+            this.peshomon.types.push(type);
+        } else {
+            this.peshomon.types = this.peshomon.types.filter((typ) => typ !== type);
+        }
     }
 }
