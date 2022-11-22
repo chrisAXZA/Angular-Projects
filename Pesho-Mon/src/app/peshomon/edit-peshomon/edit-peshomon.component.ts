429b356a985d683c2128e40fcc5f71546da5694e
chrisAXZA <nexter1@tutanota.com>
2022-11-22 12:35:57 +0200

    Implement edit-peshomon template

--- Pesho-Mon/src/app/peshomon/edit-peshomon/edit-peshomon.component.ts
+++ Pesho-Mon/src/app/peshomon/edit-peshomon/edit-peshomon.component.ts
@@ -1,16 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import Pokemon from '../pokemon';
+import PeshomonService from '../peshomon.service';
 
 @Component({
-  selector: 'app-edit-peshomon',
-  templateUrl: './edit-peshomon.component.html',
-  styles: [
-  ]
+    selector: 'app-edit-peshomon',
+    templateUrl: './edit-peshomon.component.html',
+    styleUrls: [
+    ],
 })
 export class EditPeshomonComponent implements OnInit {
+    peshomon: Pokemon;
 
-  constructor() { }
+    constructor(private route: ActivatedRoute, private peshomonService: PeshomonService) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        const peshomonId: number = Number(this.route.snapshot.paramMap.get('id'));
+        const currentPeshomon: Pokemon | undefined = this.peshomonService.getPeshomonById(peshomonId);
+
+        if (!currentPeshomon) {
+            alert(`Peshomon with id "${peshomonId}" does not exist!`);
+        }
+
+        this.peshomon = currentPeshomon!;
+    }
 
 }
