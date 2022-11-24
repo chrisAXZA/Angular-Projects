a8aa6ef781e3a7bb94ea9179119ba1a33b854851
chrisAXZA <nexter1@tutanota.com>
2022-11-24 20:25:14 +0200

    Add in-memory-data service+implement createDb

---
+++ Pesho-Mon/src/app/in-memory-data.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+
+import POKEMONS from './peshomon/pokemonList';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class InMemoryDataService implements InMemoryDbService {
+
+    constructor() { }
+
+    // default createDb function
+    // createDb(reqInfo: RequestInfo): {} | Observable<{}> | Promise<{}> { }
+
+    // will seed peshomon data for simulated REST-API
+    createDb() {
+        return { POKEMONS };
+    }
+}
