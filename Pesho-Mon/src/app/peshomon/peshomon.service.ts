4b53ee67a6d869463d4724d57c68aa92ac21fba8
chrisAXZA <nexter1@tutanota.com>
2022-11-26 19:36:06 +0200

    Implement log/handleError in peshomon.service

--- Pesho-Mon/src/app/peshomon/peshomon.service.ts
+++ Pesho-Mon/src/app/peshomon/peshomon.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { catchError, Observable, of, tap } from 'rxjs';
 
 import Pokemon from './pokemon';
-import POKEMONS from './pokemonList';
+// import POKEMONS from './pokemonList';
 
 @Injectable()
 export default class PeshomonService {
@@ -17,26 +17,31 @@ export default class PeshomonService {
     getPeshomonById(peshomonId: number): Observable<Pokemon | undefined> {
         return this.httpClient.get<Pokemon>(`api/pokemon/:${peshomonId}`)
             .pipe(
-                tap((peshomon) => console.log(peshomon),),
+                tap((peshomon) => this.log(peshomon),),
                 catchError((error) => {
-                    console.log(error);
-                    return of(undefined);
+                    // console.log(error);
+                    // return of(undefined);
+
+                    return this.handleError(error, undefined);
                 })
             );
     }
 
     // getPeshomonList(): Pokemon[] { return POKEMONS; }
+
     getPeshomonList(): Observable<Pokemon[]> {
         // HttpClient can works with data stream that can be typed, in this scenario
         // employing Angular-In-Memory to simulate a server response
         return this.httpClient.get<Pokemon[]>('api/pokemons')
             .pipe(
-                tap((peshomonList) => console.table(peshomonList),), // logs response
+                tap((peshomonList) => this.log(peshomonList),), // logs response
                 catchError((error) => {
                     // if error occurs, logs error + return Observable in order for app to
                     // keep functioning
-                    console.log(error);
-                    return of([]);
+                    // console.log(error);
+                    // return of([]);
+                    
+                    return this.handleError(error, []);
                 }),
             );
     }
@@ -55,5 +60,14 @@ export default class PeshomonService {
             'Psy',
             'Water',
         ];
+    }
+
+    private log(response: Pokemon[] | Pokemon | undefined) {
+        console.table(response);
+    }
+
+    private handleError(error: Error, defaultReturn: any) {
+        console.error(error);
+        return of(defaultReturn);
     }
 }
