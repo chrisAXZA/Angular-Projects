6b4751bfc4e73b24bdae11b9184fd65242578c4b
chrisAXZA <nexter1@tutanota.com>
2022-11-29 22:23:28 +0200

    Implement add-peshomon template

--- Pesho-Mon/src/app/peshomon/add-peshomon/add-peshomon.component.ts
+++ Pesho-Mon/src/app/peshomon/add-peshomon/add-peshomon.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import Pokemon from '../pokemon';
+
 @Component({
-  selector: 'app-add-peshomon',
-  templateUrl: './add-peshomon.component.html',
-  styles: [
-  ]
+    selector: 'app-add-peshomon',
+    templateUrl: './add-peshomon.component.html',
+    styles: [
+    ]
 })
 export class AddPeshomonComponent implements OnInit {
+    peshomon: Pokemon;
+    
+    constructor() { }
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.peshomon = new Pokemon();
+    }
 
 }
