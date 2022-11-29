0bde4d4bf56e4f09bd3c27d2920d405efd05ef2b
chrisAXZA <nexter1@tutanota.com>
2022-11-29 22:35:19 +0200

    Refactor pokemon.ts with default values

--- Pesho-Mon/src/app/peshomon/pokemon.ts
+++ Pesho-Mon/src/app/peshomon/pokemon.ts
@@ -1,9 +1,27 @@
 export default class Pokemon {
-    id: number;
-    hp: number;
-    cp: number;
-    name: string;
-    picture: string;
-    types: Array<string>;
-    created: Date;
+    constructor(
+        public id: number = 0,
+        public name: string = 'Peshomon',
+        public hp: number = 100,
+        public cp: number = 10,
+        public picture: string = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png',
+        public types: Array<string> = ['Normal'],
+        public created: Date = new Date(),
+    ) {
+        this.id = id;
+        this.hp = hp;
+        this.cp = cp;
+        this.name = name;
+        this.picture = picture;
+        this.types = types;
+        this.created = created;
+    }
+
+    // id: number;
+    // hp: number;
+    // cp: number;
+    // name: string;
+    // picture: string;
+    // types: Array<string>;
+    // created: Date;
 }
