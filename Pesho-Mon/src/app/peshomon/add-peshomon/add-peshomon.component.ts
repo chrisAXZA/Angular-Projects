afc489de64fd597cbcb57c4ff574ed398a61d665
chrisAXZA <nexter1@tutanota.com>
2022-11-30 18:41:26 +0200

    Add returnToPeshomonList btn in add-peshomon

--- Pesho-Mon/src/app/peshomon/add-peshomon/add-peshomon.component.ts
+++ Pesho-Mon/src/app/peshomon/add-peshomon/add-peshomon.component.ts
@@ -1,20 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import Pokemon from '../pokemon';
 
 @Component({
     selector: 'app-add-peshomon',
     templateUrl: './add-peshomon.component.html',
-    styles: [
-    ]
+    styleUrls: ['./add-peshomon.component.scss'],
 })
 export class AddPeshomonComponent implements OnInit {
     peshomon: Pokemon;
     
-    constructor() { }
+    constructor(private router: Router) { }
 
     ngOnInit(): void {
         this.peshomon = new Pokemon();
     }
 
+    returnToPeshomonList() {
+        this.router.navigate(['/pokemons']);
+    }
 }
