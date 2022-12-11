fe434221afcfc24bb7529b256893a00157f72a73
chrisAXZA <nexter1@tutanota.com>
2022-12-11 21:53:54 +0200

    Implement login in auth.service

--- Pesho-Mon/src/app/auth.service.ts
+++ Pesho-Mon/src/app/auth.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { delay, Observable, of, tap } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthService {
+    isLoggedIn: boolean = false;
+    redirectUrl: string;
 
     constructor() { }
+
+    login(username: string, password: string): Observable<boolean> {
+        const isLoggedIn = (username === 'pesho' && password === 'pesho123');
+
+        // delay will simulate time-lapse for given request
+        return of(isLoggedIn)
+            .pipe(
+                delay(1000),
+                tap((isLoggedIn) => this.isLoggedIn = isLoggedIn),
+            );
+    }
+
+    logout() {
+        this.isLoggedIn = false;
+    }
 }
