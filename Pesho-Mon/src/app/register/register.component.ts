a264d79bcedf772d26910ee6f14912d687293f1e
chrisAXZA <nexter1@tutanota.com>
2022-12-19 14:29:21 +0200

    Implement register template

--- Pesho-Mon/src/app/register/register.component.ts
+++ Pesho-Mon/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-register',
@@ -7,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
     ]
 })
 export class RegisterComponent implements OnInit {
+    username: string;
+    password: string;
+    email: string;
 
-    constructor() { }
+    constructor(private router: Router) { }
 
     ngOnInit(): void {
     }
 
+    cancelChanges(event: Event) {
+        event.preventDefault();
+        this.router.navigate(['/pokemons']);
+    }
+
+    onSubmit() {
+        // if (this.isAddForm) {
+        //     const peshomon = this.peshomonService.findPeshomonByName(this.peshomon.name);
+
+        //     if (peshomon) {
+        //         return alert(`Peshomon with given name "${this.peshomon.name}" already exists !`);
+        //     }
+
+        //     // back-end will create new id value and pass on to this.router
+        //     this.peshomonService.addPeshomon(this.peshomon)
+        //         .subscribe((peshomon: Pokemon) => this.router.navigate(['/pokemon', peshomon.id]));
+        // } else {
+        //     this.peshomonService.updatePeshomon(this.peshomon)
+        //         .subscribe(() => this.router.navigate(['/pokemon', this.peshomon.id]));
+        // }
+    }
 }
