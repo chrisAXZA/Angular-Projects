ccf4ee94c916877d73848deb71bdbe836a60c8ad
chrisAXZA <nexter1@tutanota.com>
2022-12-21 16:15:35 +0200

    Add imgAnim in page-not-found template

--- Pesho-Mon/src/app/page-not-found/page-not-found.component.ts
+++ Pesho-Mon/src/app/page-not-found/page-not-found.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
     selector: 'app-page-not-found',
     templateUrl: './page-not-found.component.html',
     styles: [
-    ]
+    ],
+    animations: [
+        trigger('imgAnim', [
+            transition(':enter', [
+                style({
+                    opacity: 0,
+                }),
+                animate('1s ease-in', style({
+                    opacity: 1,
+                })),
+            ]),
+        ]),
+    ],
 })
 export class PageNotFoundComponent implements OnInit {
     constructor(private router: Router) { }
